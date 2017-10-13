ca5bc53d701c0f0a75f020c2d28d6ffe6d93cb99
Martin Zlámal <mrtnzlml@gmail.com>
2017-10-13 16:01:23 -0500

    Add duration component

---
+++ app/screens/search/Duration.js
@@ -0,0 +1,23 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+type Props = {
+  minutes: number,
+};
+
+export default function Duration({ minutes }: Props) {
+  if (minutes < 0) {
+    minutes = 0;
+  }
+
+  const hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+
+  return (
+    <Text>
+      {hours}h {minutes}m
+    </Text>
+  );
+}
