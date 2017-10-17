5ade73b27548c5081c8c713f79d8d455a7ceb73e
Martin Zlámal <mrtnzlml@gmail.com>
2017-10-17 14:39:05 -0500

    Add Booking screen

---
+++ app/screens/booking/Booking.js
@@ -0,0 +1,14 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default class Booking extends React.PureComponent {
+  static navigationOptions = {
+    title: 'Booking detail - 1234567',
+  };
+
+  render = () => {
+    return <Text>You will see details of the booking here.</Text>;
+  };
+}
