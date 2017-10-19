706ca7203c109c7371673fed7532eac2da317997
Martin Zlámal <mrtnzlml@gmail.com>
2017-10-19 18:03:12 -0500

    Add profile page (with tab navigator)

---
+++ app/screens/profile/Profile.js
@@ -0,0 +1,16 @@
+// @flow
+
+import * as React from 'react';
+import { Text, ScrollView } from 'react-native';
+
+export default class Booking extends React.PureComponent<{}, {}> {
+  render = () => {
+    return (
+      <ScrollView>
+        <Text>TODO: basic user info</Text>
+        <Text>TODO: logout</Text>
+        <Text>TODO: login if not logged</Text>
+      </ScrollView>
+    );
+  };
+}
