875a3fead37c35f36e79f71d6de476ba3a98947d
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-03 15:28:47 -0600

    Animate search header

--- app/screens/homepage/search/SearchHeader.js
+++ app/screens/homepage/search/SearchHeader.js
@@ -1,9 +1,16 @@
 // @flow
 
 import * as React from 'react';
-import { Animated, StyleSheet, View, Platform, StatusBar } from 'react-native';
+import {
+  Animated,
+  Platform,
+  StatusBar,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native';
 
 import SearchForm from './SearchForm';
+import config from '../../../config/application';
 
 type Props = {
   onSend: ({
@@ -11,41 +18,91 @@ type Props = {
     to: string,
     date: Date,
   }) => void,
+  onToggle: () => void,
 };
 
-export default class SearchHeader extends React.PureComponent<Props, {}> {
+type AnimatedState = {
+  top: number,
+  right: number,
+  left: number,
+  height: number,
+};
+
+type State = AnimatedState & {
+  expanded: boolean,
+};
+
+const shrinkedValues = ({
+  top: Platform.OS === 'ios' ? 40 : StatusBar.currentHeight,
+  right: 20,
+  left: 20,
+  height: 50,
+}: AnimatedState);
+
+const expandedValues = ({
+  top: 0,
+  right: 0,
+  left: 0,
+  height: 200,
+}: AnimatedState);
+
+export default class SearchHeader extends React.PureComponent<Props, State> {
   state = {
-    height: new Animated.Value(50),
     expanded: false,
+    top: new Animated.Value(shrinkedValues.top),
+    right: new Animated.Value(shrinkedValues.right),
+    left: new Animated.Value(shrinkedValues.left),
+    height: new Animated.Value(shrinkedValues.height),
+  };
+
+  animate = (attributes: $Keys<AnimatedState>[]) => {
+    attributes.forEach(attribute => {
+      Animated.timing(this.state[attribute], {
+        toValue: this.state.expanded
+          ? expandedValues[attribute]
+          : shrinkedValues[attribute],
+        duration: config.animations.duration,
+      }).start();
+    });
+  };
+
+  toggleHeader = () => {
+    this.setState({ expanded: !this.state.expanded }, () => {
+      this.props.onToggle();
+      this.animate(['top', 'right', 'left', 'height']);
+    });
   };
 
   render = () => (
-    <View style={styles.container} behavior="padding">
-      {/* TODO: this.state.expanded */}
-      <SearchForm
-        onSend={(from, to, date) =>
-          this.props.onSend({
-            from,
-            to,
-            date,
-          })}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={this.toggleHeader}>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          top: this.state.top,
+          right: this.state.right,
+          left: this.state.left,
+          height: this.state.height,
+          backgroundColor: 'white',
+          shadowColor: 'black',
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.3,
+          padding: 10,
+        }}
+      >
+        {this.state.expanded ? (
+          <SearchForm
+            onSend={(from, to, date) =>
+              this.props.onSend({
+                from,
+                to,
+                date,
+              })}
+          />
+        ) : (
+          <Text>Where do you want to travel?</Text>
+        )}
+      </Animated.View>
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    zIndex: 1,
-    top: Platform.OS === 'ios' ? 40 : StatusBar.currentHeight,
-    right: 10,
-    left: 10,
-    margin: 10,
-    backgroundColor: 'white',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    padding: 10,
-  },
-});
