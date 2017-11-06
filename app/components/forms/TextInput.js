e2c5bdd1c3f7fe3bdcd04a5fa21f50c306a0eb8c
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-06 12:51:03 -0600

    Add styles for DatePicker

--- app/components/forms/TextInput.js
+++ app/components/forms/TextInput.js
@@ -3,29 +3,29 @@
 import * as React from 'react';
 import { TextInput as OriginalTextInput, View } from 'react-native';
 
-export const colours = {
-  text: 'black',
-  background: 'transparent',
+export const styles = {
+  input: {
+    color: 'black',
+    backgroundColor: 'transparent',
+    fontSize: 15,
+    padding: 10,
+    flex: 1,
+  },
+  wrapper: {
+    height: 40,
+    backgroundColor: '#ededed',
+    marginBottom: 5,
+  }
 };
 
 export default function TextInput(props: Object) {
   return (
-    <View
-      style={{
-        height: 40,
-        backgroundColor: '#ededed',
-        padding: 10,
-        marginBottom: 5,
-      }}
-    >
+    <View style={styles.wrapper}>
       <OriginalTextInput
         underlineColorAndroid="transparent"
         {...props}
         style={{
-          flex: 1,
-          color: colours.text,
-          backgroundColor: colours.background,
-          fontSize: 15,
+          ...styles.input,
         }}
       />
     </View>
