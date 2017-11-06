392ebaba1048d022db54df373149ee3ae5c838ad
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-06 13:18:28 -0600

    GoogleButton: reuse 'Button' component

--- app/components/visual/buttons/Button.js
+++ app/components/visual/buttons/Button.js
@@ -8,19 +8,35 @@ import Color from '../../../styles/Color';
 type Props = {
   onPress: Function,
   title: string,
+  styles?: {
+    button?: Object,
+    buttonText?: Object,
+  },
+  touchable?: boolean,
 };
 
-export default function Button({ onPress, title }: Props) {
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{title}</Text>
-      </View>
-    </TouchableOpacity>
+export default function Button({
+  onPress,
+  title,
+  styles,
+  touchable = true,
+}: Props) {
+  const buttonView = (
+    <View style={[defaultStyles.button, styles && styles.button]}>
+      <Text style={[defaultStyles.buttonText, styles && styles.buttonText]}>
+        {title}
+      </Text>
+    </View>
   );
+
+  if (touchable) {
+    return <TouchableOpacity onPress={onPress}>{buttonView}</TouchableOpacity>;
+  } else {
+    return buttonView;
+  }
 }
 
-const styles = StyleSheet.create({
+const defaultStyles = StyleSheet.create({
   button: {
     justifyContent: 'center',
     alignItems: 'center',
--- app/components/visual/buttons/__tests__/GoogleButton.test.js
+++ app/components/visual/buttons/__tests__/GoogleButton.test.js
@@ -6,11 +6,15 @@ import renderer from 'react-test-renderer';
 import GoogleButton from '../GoogleButton';
 
 it('renders as expected', () => {
-  const rendered = renderer.create(<GoogleButton loading={false} />).toJSON();
+  const rendered = renderer
+    .create(<GoogleButton loading={false} onPress={() => Promise.resolve()} />)
+    .toJSON();
   expect(rendered).toMatchSnapshot();
 });
 
 it('renders as expected while loading', () => {
-  const rendered = renderer.create(<GoogleButton loading={true} />).toJSON();
+  const rendered = renderer
+    .create(<GoogleButton loading={true} onPress={() => Promise.resolve()} />)
+    .toJSON();
   expect(rendered).toMatchSnapshot();
 });
