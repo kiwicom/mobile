318ec8c567ecec84bb0f41b352e47d3ae1e63465
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-10 10:49:07 -0600

    Add Sentry logging
    Closes #1

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 // @flow
 
+import Sentry from 'sentry-expo';
+
+import config from './app/config/application';
+
+Sentry.enableInExpoDevelopment = config.sentry.enableInExpoDevelopment;
+Sentry.config(config.sentry.dsn).install();
+
 export { default } from './app/screens';
