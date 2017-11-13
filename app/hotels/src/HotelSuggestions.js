ae1e87a851451ab3b5cf9352b60a95160effab4a
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-13 17:05:45 -0600

    Add 'HotelsSuggestions' component as a PoC of this approach
    This component is separate package in the monorepo so it can be
    easily installed in different applications (e.g. in native code).

---
+++ app/hotels/src/HotelSuggestions.js
@@ -0,0 +1,8 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function HotelSuggestions() {
+  return <Text>TODO: hotel suggestions</Text>;
+}
--- app/native/src/screens/homepage/Homepage.js
+++ app/native/src/screens/homepage/Homepage.js
@@ -2,8 +2,10 @@
 
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
+import HotelSuggestions from '@kiwicom/native-hotels';
 
 import FlightsSearchOverlay from './search/FlightsSearchOverlay';
+import CenteredView from '../../components/visual/view/CenteredView';
 
 import type { Navigation } from '../../types/Navigation';
 
@@ -16,6 +18,9 @@ export default class Homepage extends React.Component<Props, {}> {
     return (
       <View style={styles.container}>
         <FlightsSearchOverlay navigation={this.props.navigation} />
+        <CenteredView>
+          <HotelSuggestions />
+        </CenteredView>
       </View>
     );
   };
