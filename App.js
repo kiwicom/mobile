88f522760b61aa70c471e62f8a3d7ba0845d382a
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-29 16:01:51 -0600

    Automatically deploy PR to the Expo (for easier reviews)

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import Sentry from 'sentry-expo';
 
 import config from './app/native/config/application';
 
-Sentry.enableInExpoDevelopment = config.sentry.enableInExpoDevelopment;
-Sentry.config(config.sentry.dsn).install();
+if (config.sentry.dsn) {
+  Sentry.enableInExpoDevelopment = config.sentry.enableInExpoDevelopment;
+  Sentry.config(config.sentry.dsn).install();
+}
 
 export { default } from './app/native/src/navigation';
