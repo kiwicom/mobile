38cf786f52b1f7ec61e179c758951234dc432349
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-30 12:33:11 -0600

    Polish Expo publisher

--- scripts/expo/index.js
+++ scripts/expo/index.js
@@ -1,15 +1,22 @@
 #!/usr/bin/env node
 
+if (process.env.TRAVIS !== 'true') {
+  console.error('This script can be executed only in Travis CI environment.');
+  process.exit(1);
+}
+
 const spawn = require('./spawn');
-
+const env = require('./environment');
 const publishCodeReview = require('./commands/publishCodeReview');
-
-const expUsername = process.env.EXP_USERNAME;
-const expPassword = process.env.EXP_PASSWORD;
 
 spawn(
   'yarn',
-  ['exp', 'login', '-u', expUsername, '-p', expPassword],
+  [
+    'exp',
+    'login',
+    '--username', env.EXP_USERNAME,
+    '--password', env.EXP_PASSWORD,
+  ],
   loginError => {
     if (loginError) {
       throw new Error(loginError);
