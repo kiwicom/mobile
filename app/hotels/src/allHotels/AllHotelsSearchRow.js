8250b48dfe4dbd86f104888d2d057dfa9bdc2646
Vaclav Sir <vaclav.sir@gmail.com>
2017-12-19 17:40:23 +0100

    Pass hotel ID from the list to the detail

--- app/hotels/src/allHotels/AllHotelsSearchRow.js
+++ app/hotels/src/allHotels/AllHotelsSearchRow.js
@@ -14,45 +14,52 @@ import {
 import type { AllHotelsSearchRow as AllHotelsSearchRowProps } from './__generated__/AllHotelsSearchRow.graphql';
 
 type Props = {
-  onGoToSingleHotel: () => void,
+  openSingleHotel: (id: string) => void,
   data: AllHotelsSearchRowProps,
 };
 
-function AllHotelsSearchRow({ onGoToSingleHotel, data }: Props) {
-  const price = {
-    ...{
-      // default null object
-      amount: null,
-      currency: null,
-    },
-    ...data.price,
+class AllHotelsSearchRow extends React.Component<Props> {
+  onGoToSingleHotel = () => {
+    this.props.openSingleHotel(this.props.data.id);
   };
-  const thumbnailUrl = idx(data, _ => _.mainPhoto.thumbnailUrl);
 
-  return (
-    <SimpleCard
-      separator={false}
-      onPress={onGoToSingleHotel}
-      additionalStyles={{ marginTop: 5, flex: 1, flexDirection: 'row' }}
-    >
-      <View style={{ paddingHorizontal: 10 }}>
-        <NetworkImage
-          style={{ width: 50, height: 75, borderRadius: 2 }}
-          resizeMode="cover"
-          source={{ uri: thumbnailUrl }}
-        />
-      </View>
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
-        <Text>TODO (needs GraphQL update)</Text>
-        <Price
-          amount={price.amount}
-          currency={price.currency}
-          style={{ fontWeight: 'bold', color: Color.brand }}
-        />
-      </View>
-    </SimpleCard>
-  );
+  render() {
+    const { data } = this.props;
+    const price = {
+      ...{
+        // default null object
+        amount: null,
+        currency: null,
+      },
+      ...data.price,
+    };
+    const thumbnailUrl = idx(data, _ => _.mainPhoto.thumbnailUrl);
+
+    return (
+      <SimpleCard
+        separator={false}
+        onPress={this.onGoToSingleHotel}
+        additionalStyles={{ marginTop: 5, flex: 1, flexDirection: 'row' }}
+      >
+        <View style={{ paddingHorizontal: 10 }}>
+          <NetworkImage
+            style={{ width: 50, height: 75, borderRadius: 2 }}
+            resizeMode="cover"
+            source={{ uri: thumbnailUrl }}
+          />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontWeight: 'bold' }}>{data.name}</Text>
+          <Text>TODO (needs GraphQL update)</Text>
+          <Price
+            amount={price.amount}
+            currency={price.currency}
+            style={{ fontWeight: 'bold', color: Color.brand }}
+          />
+        </View>
+      </SimpleCard>
+    );
+  }
 }
 
 export default createFragmentContainer(
