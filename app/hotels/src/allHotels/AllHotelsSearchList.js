b6ae2c8d7bd72a2913cf2facd690e03da066449e
Vaclav Sir <vaclav.sir@gmail.com>
2017-12-27 15:40:28 +0100

    Show an error when all hotels query fails

--- app/hotels/src/allHotels/AllHotelsSearchList.js
+++ app/hotels/src/allHotels/AllHotelsSearchList.js
@@ -3,7 +3,8 @@
 import * as React from 'react';
 import idx from 'idx';
 import { createFragmentContainer, graphql } from 'react-relay';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { CenteredView } from '@kiwicom/react-native-app-common';
 
 import AllHotelsSearchRow from './AllHotelsSearchRow';
 
@@ -14,24 +15,33 @@ type Props = {|
   data: AllHotelsSearchListProps,
 |};
 
-function AllHotelsSearchList(combinedProps: Props) {
-  const hotels = idx(combinedProps, _ => _.data.edges) || []; // TODO: render 404 page instead of nothing
-  return (
-    <ScrollView>
-      {hotels.map(edge => {
-        if (edge) {
-          const { node: hotel } = edge;
-          return (
-            <AllHotelsSearchRow
-              data={hotel}
-              openSingleHotel={combinedProps.openSingleHotel}
-              key={hotel && hotel.id}
-            />
-          );
-        }
-      })}
-    </ScrollView>
-  );
+export function AllHotelsSearchList(combinedProps: Props) {
+  const hotels = idx(combinedProps, _ => _.data.edges) || [];
+
+  if (hotels.length === 0) {
+    return (
+      <CenteredView>
+        <Text>No hotels found</Text>
+      </CenteredView>
+    );
+  } else {
+    return (
+      <ScrollView>
+        {hotels.map(edge => {
+          if (edge) {
+            const { node: hotel } = edge;
+            return (
+              <AllHotelsSearchRow
+                data={hotel}
+                openSingleHotel={combinedProps.openSingleHotel}
+                key={hotel && hotel.id}
+              />
+            );
+          }
+        })}
+      </ScrollView>
+    );
+  }
 }
 
 export default createFragmentContainer(
