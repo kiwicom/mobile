30a7041add1cf317aa8c9fbf11d316c2095e15cf
Martin Zlámal <mrtnzlml@gmail.com>
2018-01-09 17:11:48 +0700

    Expo: remove 'commentOnGitHub' feature
    I am still keeping the publish command there because I want to deploy
    at least the master branch until we remove it completely.

--- scripts/expo/commands/publishCodeReview.js
+++ scripts/expo/commands/publishCodeReview.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 const spawn = require('../spawn');
 const utils = require('../utils');
 const env = require('../environment');
@@ -7,31 +5,6 @@ const env = require('../environment');
 function preparePublishName() {
   const branchName = env.CI_BRANCH;
   return `code-review-${branchName}`.replace(/[^a-zA-Z0-9\\-]/, '-');
-}
-
-async function commentOnGitHub(buildName, githubPullRequestId) {
-  const expUrl = `exp://exp.host/@${env.EXP_USERNAME}/${buildName}`;
-  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${expUrl}`;
-  const issueUrl = `https://${env.GITHUB_USERNAME}:${env.GITHUB_TOKEN}@api.github.com/repos/${env.CI_PROJECT_REPONAME}/issues/${githubPullRequestId}/comments`;
-
-  const body = `
-  Please test these changes in [Expo](https://docs.expo.io/versions/latest/introduction/installation.html#mobile-client-expo-for-ios-and-android) after build has finished:
-
-  ![QR Code](${qrUrl})
-
-  \`${expUrl}\`
-  `;
-
-  const comments = await (await fetch(issueUrl)).json();
-  if (!comments.find(comment => comment.body === body)) {
-    // create comment with the QR code because it doesn't exist yet
-    await fetch(issueUrl, {
-      method: 'POST',
-      headers: { 'User-Agent': 'ci' },
-      body: JSON.stringify({ body }),
-    });
-    console.log('GitHub comment created.');
-  }
 }
 
 module.exports = () => {
@@ -48,14 +21,9 @@ module.exports = () => {
     }),
   });
 
-  const githubPullRequestId = env.CI_PULL_REQUEST_ID;
   spawn('yarn', ['exp', 'publish'], async publishError => {
     if (publishError) {
       throw new Error(publishError);
-    } else if (githubPullRequestId > 0) {
-      // comments only when the PR is opened (but publish always)
-      console.log(`Trying to comment PR #${githubPullRequestId}...`);
-      await commentOnGitHub(slug, githubPullRequestId);
     }
   });
 };
