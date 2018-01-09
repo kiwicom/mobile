188f190a577cd56e4501ef068847fa23c63e4cc5
Martin Zlámal <mrtnzlml@gmail.com>
2018-01-09 17:11:48 +0700

    Expo: remove 'commentOnGitHub' feature
    I am still keeping the publish command there because I want to deploy
    at least the master branch until we remove it completely.

--- scripts/expo/environment.js
+++ scripts/expo/environment.js
@@ -8,6 +8,4 @@ module.exports = {
   // Continuous Integration:
   // see: https://circleci.com/docs/2.0/env-vars/
   CI_BRANCH: process.env.CIRCLE_BRANCH,
-  CI_PULL_REQUEST_ID: (process.env.CIRCLE_PULL_REQUEST || '').split('/').slice(-1)[0] || 0,
-  CI_PROJECT_REPONAME: process.env.CIRCLE_PROJECT_USERNAME + '/' + process.env.CIRCLE_PROJECT_REPONAME
 };
