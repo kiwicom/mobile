ef167138b5abd379277b439eea067ce3675825c2
Martin Zlámal <mrtnzlml@gmail.com>
2018-01-09 10:17:02 +0700

    Add simple Playbook for better components development

--- app/App.js
+++ app/App.js
@@ -1,12 +1,3 @@
 // @flow
 
-// import Sentry from 'sentry-expo';
-//
-// import config from './app/core/config/application';
-//
-// if (config.sentry.dsn) {
-//   Sentry.enableInExpoDevelopment = config.sentry.enableInExpoDevelopment;
-//   Sentry.config(config.sentry.dsn).install();
-// }
-//
 export { default } from './core/src/navigation';
