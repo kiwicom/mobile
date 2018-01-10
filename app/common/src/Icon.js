3b054bcee21a3fc40894b38dc197e9131baa4146
Trond Bergquist <trond_bergquist@hotmail.com>
2018-01-10 09:16:57 +0100

    Add react-native-vector-icons to icon component

--- app/common/src/Icon.js
+++ app/common/src/Icon.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react';
 import { Text } from 'react-native';
-// import { MaterialIcons } from '@expo/vector-icons';
-// import { Color } from '@kiwicom/react-native-app-common';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { Color } from '@kiwicom/react-native-app-common';
 
 type Props = {|
   name: string,
@@ -14,16 +14,15 @@ type Props = {|
   style?: Object,
 |};
 
-// const defaults = {
-//   color: Color.grey.$600,
-// };
+ const defaults = {
+   color: Color.grey.$600,
+ };
 
 /**
  * Currently only supported package is "MaterialIcons".
  * @see https://material.io/icons/
  */
 export default function Icon(props: Props) {
-  return <Text>??</Text>;
-  // const newProps = { ...defaults, ...props };
-  // return <MaterialIcons {...newProps} />;
+  const newProps = { ...defaults, ...props };
+  return <MaterialIcons {...newProps} />;
 }
