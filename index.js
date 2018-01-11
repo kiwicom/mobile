e8fb28a6e50e7ef10fe503e5b654a78480daafa0
Trond Bergquist <trond_bergquist@hotmail.com>
2018-01-11 10:57:23 +0100

    Add native module ios/android example

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { AppRegistry } from 'react-native';
 
-import App from './app/App';
+// import App from './app/App';
+import NativeModuleApp from './app/examples/native-module/NativeModuleApp';
 
-AppRegistry.registerComponent('reactNativeApp', () => App);
+AppRegistry.registerComponent('reactNativeApp', () => NativeModuleApp);
