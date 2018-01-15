e3291bd42b7685b1271bcd2d4e75651daff23286
Trond Bergquist <trond_bergquist@hotmail.com>
2018-01-15 11:57:19 +0100

    Add hotels package to native app

---
+++ iosExample/iosExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosExample
+//
+//  Created by Trond Bergquist on 15.01.2018.
+//  Copyright © 2018 com.kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosExample/iosExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosExample
+//
+//  Created by Trond Bergquist on 15.01.2018.
+//  Copyright © 2018 com.kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
