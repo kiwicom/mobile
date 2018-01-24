3695cb06f558ab19f089e88ceb3d43aa881e98b1
Martin Zlámal <mrtnzlml@gmail.com>
2018-01-24 17:59:03 +0700

    Use CurrencyFormatter in Price component
    The only problem is that I don't really know how the formatter works
    because there are not enough details in the specification.
    
    See: https://confluence.kiwi.com/display/MOB/Semi+native+hotels#Seminativehotels-Currencyratesandformatters

--- app/common/src/Price.js
+++ app/common/src/Price.js
@@ -3,15 +3,15 @@
 import * as React from 'react';
 import { Text } from 'react-native';
 
+import CurrencyFormatter from './services/CurrencyFormatter';
+import type { StylePropType } from '../types/Styles';
+
 type Props = {|
   amount: number | null, // may be null (compatible with GraphQL query failure)
   currency: string | null, // may be null (compatible with GraphQL query failure)
-  // $FlowFixMeProps
-  style?: Object,
-  // $FlowFixMeProps
-  amountStyle?: Object,
-  // $FlowFixMeProps
-  currencyStyle?: Object,
+  style?: StylePropType,
+  amountStyle?: StylePropType,
+  currencyStyle?: StylePropType,
 |};
 
 /**
@@ -20,9 +20,10 @@ type Props = {|
  * all prices should be wrapped in this component so the future changes are easy.
  */
 export default function Price(props: Props) {
+  const amount = props.amount ? CurrencyFormatter(props.amount) : '';
   return (
     <Text style={props.style}>
-      <Text style={props.amountStyle}>{props.amount}</Text>{' '}
+      <Text style={props.amountStyle}>{amount}</Text>{' '}
       <Text style={props.currencyStyle}>{props.currency}</Text>
     </Text>
   );
