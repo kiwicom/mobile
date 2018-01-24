c660460c19d2b8db037e5895466996281dee9613
Martin Zlámal <mrtnzlml@gmail.com>
2018-01-24 17:42:19 +0700

    Log real events in hotels package
    Note - we do not have information about `hasActiveBooking`.
    
    See: https://confluence.kiwi.com/display/MOB/Semi+native+hotels#Seminativehotels-Logging

--- app/hotels/src/allHotels/AllHotelsSearchList.js
+++ app/hotels/src/allHotels/AllHotelsSearchList.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import idx from 'idx';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { ScrollView, Text } from 'react-native';
-import { CenteredView } from '@kiwicom/react-native-app-common';
+import { CenteredView, Logger } from '@kiwicom/react-native-app-common';
 
 import AllHotelsSearchRow from './AllHotelsSearchRow';
 import type { AllHotelsSearchList as AllHotelsSearchListProps } from './__generated__/AllHotelsSearchList.graphql';
@@ -14,33 +14,42 @@ type Props = {|
   data: AllHotelsSearchListProps,
 |};
 
-export function AllHotelsSearchList(combinedProps: Props) {
-  const hotels = idx(combinedProps, _ => _.data.edges) || [];
+export class AllHotelsSearchList extends React.Component<Props> {
+  componentDidMount = () => {
+    Logger.LogEvent(Logger.Event.Displayed, Logger.Category.Ancillary, {
+      type: 'Hotels',
+      step: 'results',
+    });
+  };
 
-  if (hotels.length === 0) {
-    return (
-      <CenteredView>
-        <Text>No hotels found</Text>
-      </CenteredView>
-    );
-  } else {
-    return (
-      <ScrollView>
-        {hotels.map(edge => {
-          if (edge) {
-            const { node: hotel } = edge;
-            return (
-              <AllHotelsSearchRow
-                data={hotel}
-                openSingleHotel={combinedProps.openSingleHotel}
-                key={hotel && hotel.id}
-              />
-            );
-          }
-        })}
-      </ScrollView>
-    );
-  }
+  render = () => {
+    const hotels = idx(this.props, _ => _.data.edges) || [];
+
+    if (hotels.length === 0) {
+      return (
+        <CenteredView>
+          <Text>No hotels found</Text>
+        </CenteredView>
+      );
+    } else {
+      return (
+        <ScrollView>
+          {hotels.map(edge => {
+            if (edge) {
+              const { node: hotel } = edge;
+              return (
+                <AllHotelsSearchRow
+                  data={hotel}
+                  openSingleHotel={this.props.openSingleHotel}
+                  key={hotel && hotel.id}
+                />
+              );
+            }
+          })}
+        </ScrollView>
+      );
+    }
+  };
 }
 
 export default createFragmentContainer(
