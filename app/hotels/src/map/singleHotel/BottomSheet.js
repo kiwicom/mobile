82629dac66dc567bc4e34df9688f3b3ce0d59828
Vaclav Sir <vaclav.sir@gmail.com>
2018-01-30 02:21:18 +0400

    Extract the bottom sheet dimensions
    Not sure how to DRY, we need the width in HotelSwipeList also for the carousel.

---
+++ app/hotels/src/map/singleHotel/BottomSheet.js
@@ -0,0 +1,59 @@
+// @flow
+
+import * as React from 'react';
+import { View } from 'react-native';
+import { Dimensions } from 'react-native';
+import {
+  BottomSheet as CommonBottomSheet,
+  Device,
+  type OnDimensionsChange,
+} from '@kiwicom/react-native-app-common';
+
+import { getWidth, openHeight, closedHeight } from '../bottomSheetDimensions';
+
+type Props = {|
+  children: React.Node,
+|};
+
+type State = {|
+  screenWidth: number,
+|};
+
+export default class BottomSheet extends React.Component<Props, State> {
+  isTablet: boolean;
+
+  constructor(props: Props) {
+    super(props);
+    this.isTablet = Device.isTablet();
+
+    this.state = {
+      screenWidth: Dimensions.get('screen').width,
+    };
+  }
+
+  componentDidMount = () => {
+    Dimensions.addEventListener('change', this.onDimensionsChanged);
+  };
+
+  componentWillUnmount = () => {
+    Dimensions.removeEventListener('change', this.onDimensionsChanged);
+  };
+
+  onDimensionsChanged = ({ screen: { width } }: OnDimensionsChange) => {
+    this.setState({ screenWidth: width });
+  };
+
+  getWidth = () => getWidth(this.state.screenWidth, this.isTablet);
+
+  render = () => {
+    const { children } = this.props;
+
+    return (
+      <View style={{ width: this.getWidth() }}>
+        <CommonBottomSheet openHeight={openHeight} closedHeight={closedHeight}>
+          {children}
+        </CommonBottomSheet>
+      </View>
+    );
+  };
+}
