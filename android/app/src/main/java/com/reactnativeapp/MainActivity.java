7491c8ce4c4689b5eec2e913158810c3841be7bb
Radek Bartoň <radek.barton@kiwi.com>
2018-02-01 09:56:27 +0100

    Android: add logging library

--- android/app/src/main/java/com/reactnativeapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativeapp/MainActivity.java
@@ -1,6 +1,13 @@
 package com.reactnativeapp;
 
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactActivity;
+import com.trinerdis.skypicker.logging.configuration.DeviceInfo;
+import com.trinerdis.skypicker.logging.sender.EventSender;
+
+import java.util.UUID;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +18,23 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "reactNativeApp";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setupEventSender();
+    }
+
+    private void setupEventSender() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        String visitorId = UUID.randomUUID().toString();
+        DeviceInfo deviceInfo = new DeviceInfo();
+
+        EventSender.defaultInitialize(
+                getApplication(), deviceInfo, new String[]{}, visitorId, BuildConfig.DEBUG
+        );
     }
 }
