7224149388e97e78c6c8578177dbd661f77ba81f
Radek Bartoň <radek.barton@kiwi.com>
2018-02-08 14:42:50 +0100

    Simplified logging library initialization.

--- android/app/src/main/java/com/reactnativeapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativeapp/MainActivity.java
@@ -4,14 +4,8 @@ import android.os.Bundle;
 
 import com.facebook.FacebookSdk;
 import com.facebook.react.ReactActivity;
-import com.trinerdis.skypicker.logging.configuration.Configuration;
-import com.trinerdis.skypicker.logging.event.model.AppConfig;
-import com.trinerdis.skypicker.logging.sender.AppsFlyerEventSender;
-import com.trinerdis.skypicker.logging.sender.CommonExponeaEventSender;
+import com.trinerdis.skypicker.logging.configuration.DeviceInfo;
 import com.trinerdis.skypicker.logging.sender.EventSender;
-import com.trinerdis.skypicker.logging.sender.FacebookEventSender;
-import com.trinerdis.skypicker.logging.sender.LogmoleEventSender;
-import com.trinerdis.skypicker.logging.sender.MobileExponeaEventSender;
 
 import java.util.UUID;
 
@@ -34,38 +28,13 @@ public class MainActivity extends ReactActivity {
     }
 
     private void setupEventSender() {
-
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-        Configuration configuration = Configuration.getConfiguration(this);
         String visitorId = UUID.randomUUID().toString();
-        AppConfig appConfig = new AppConfig(false, "0.1", 1);
+        DeviceInfo deviceInfo = new DeviceInfo();
 
-        AppsFlyerEventSender appsFlyerEventSender = new AppsFlyerEventSender(
-                getApplication(), configuration, BuildConfig.DEBUG, visitorId
+        EventSender.defaultInitialize(
+                getApplication(), deviceInfo, new String[]{}, visitorId, BuildConfig.DEBUG
         );
-
-        CommonExponeaEventSender commonExponeaEventSender = new CommonExponeaEventSender(
-                this, appConfig, new String[0], configuration, BuildConfig.DEBUG
-        );
-
-        MobileExponeaEventSender mobileExponeaEventSender = new MobileExponeaEventSender(
-                this, appConfig, new String[0], configuration, BuildConfig.DEBUG
-        );
-
-        FacebookEventSender facebookEventSender = new FacebookEventSender(
-                this, configuration, appConfig, BuildConfig.DEBUG
-        );
-
-        LogmoleEventSender logmoleEventSender = new LogmoleEventSender(
-                this, appConfig, configuration, BuildConfig.DEBUG
-        );
-
-        EventSender eventSender = EventSender.getInstance();
-        eventSender.addEventSender(appsFlyerEventSender);
-        eventSender.addEventSender(commonExponeaEventSender);
-        eventSender.addEventSender(mobileExponeaEventSender);
-        eventSender.addEventSender(facebookEventSender);
-        eventSender.addEventSender(logmoleEventSender);
     }
 }
