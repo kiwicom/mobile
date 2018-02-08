b7600e7cfc55571028d5b5f1cb330c0f96fdfe14
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-08 11:01:55 +0200

    TextInput: disable auto-correct behavior

--- app/shared/src/forms/TextInput.js
+++ app/shared/src/forms/TextInput.js
@@ -38,6 +38,7 @@ export default function TextInput(props: Props) {
       )}
       <OriginalTextInput
         underlineColorAndroid="transparent"
+        autoCorrect={false}
         {...props}
         style={stylesheetStyles.input}
       />
