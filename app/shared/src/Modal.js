86dfb4cac1b6126e25f79d36eb5f65d9e622bce6
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-13 14:22:13 +0200

    Modal: speedup in/out animations
    Hopefully, this will solve the "it's laggy" complains. Otherwise I have
    no idea what and how to fix it...

--- app/shared/src/Modal.js
+++ app/shared/src/Modal.js
@@ -15,7 +15,10 @@ type Props = {
 export default function Modal(props: Props) {
   return (
     <ReactModal
+      // NOTE: do not use native driver (see: https://github.com/react-native-community/react-native-modal/issues/92)
       supportedOrientations={['portrait', 'landscape']} // iOS only
+      animationInTiming={150}
+      animationOutTiming={150}
       {...props}
     />
   );
