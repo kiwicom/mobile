93e3b457e40e4524561f02a90ecdc8f059c4b86b
Radek Pištělák <radek.pistelak@kiwi.com>
2018-02-13 13:21:44 +0100

    Added RNLogging via npm

--- ios/SampleView.m
+++ ios/SampleView.m
@@ -2,6 +2,8 @@
 #import <React/RCTViewManager.h>
 #import "SampleView.h"
 
+#import <RNLogging/RNLoggingModule.h>
+
 @interface RCT_EXTERN_MODULE(SampleViewManager, RCTViewManager)
   RCT_EXPORT_VIEW_PROPERTY(text, NSString)
 @end
