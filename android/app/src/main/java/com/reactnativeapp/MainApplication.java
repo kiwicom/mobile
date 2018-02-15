5dae9e049cc176d57c421f880981185ddcdb0be2
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-15 16:02:30 +0200

    Remove 'react-native-svg' dependency
    Reverts https://github.com/kiwicom/react-native-app/commit/afcda293d4bd94bc68e686f2d97b25beef1c71c1

--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -3,7 +3,6 @@ package com.reactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new SvgPackage(),
                     new VectorIconsPackage(),
                     new MapsPackage(),
                     new MyNativeModulePackage()
