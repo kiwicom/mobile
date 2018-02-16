6592de1e830d8b61e833e6d8f153970337ff6ca3
Trond Bergquist <trond_bergquist@hotmail.com>
2018-02-16 10:11:07 +0100

    Hotels: use native navigation module when defined

---
+++ app/core/src/screens/HotelsPackageWrapper.js
@@ -0,0 +1,37 @@
+// @flow
+
+import * as React from 'react';
+import { NativeModules } from 'react-native';
+import HotelsStandalonePackage from '@kiwicom/react-native-app-hotels';
+import { type NavigationType } from '@kiwicom/react-native-app-navigation';
+
+import Config from '../../config/application';
+
+type Props = {|
+  navigation: NavigationType,
+|};
+
+export default class HotelsPackageWrapper extends React.Component<Props> {
+  goToHomepage = () => this.props.navigation.goBack();
+
+  handleNavigation = () => {
+    if (NativeModules.RNNavigationModule) {
+      NativeModules.RNNavigationModule.leaveHotels();
+    } else {
+      this.goToHomepage();
+    }
+  };
+
+  render = () => {
+    const affiliate = String(Config.affiliate.bookingCom);
+    return (
+      <HotelsStandalonePackage
+        bookingComAffiliate={affiliate}
+        language="en"
+        currency="EUR" // Only EUR is now fully supported as PriceFilter can't handle anything but EUR
+        onBackClicked={this.handleNavigation}
+        dataSaverEnabled={false}
+      />
+    );
+  };
+}
