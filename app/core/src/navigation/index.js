67ba259ee6f494fbfde95add408e1e93b6a8a218
Trond Bergquist <trond_bergquist@hotmail.com>
2018-02-16 10:11:07 +0100

    Hotels: use native navigation module when defined

--- app/core/src/navigation/index.js
+++ app/core/src/navigation/index.js
@@ -3,16 +3,10 @@
 import * as React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { ReduxContext } from '@kiwicom/react-native-app-redux';
-import HotelsStandalonePackage from '@kiwicom/react-native-app-hotels';
-import { type NavigationType } from '@kiwicom/react-native-app-navigation';
 
-import Config from '../../config/application';
 import HomepageStack from './HomepageStack';
 import UserReducer from '../services/redux/UserReducer';
-
-type Props = {|
-  navigation: NavigationType,
-|};
+import HotelsPackageWrapper from '../screens/HotelsPackageWrapper';
 
 const Navigation = StackNavigator(
   {
@@ -20,26 +14,11 @@ const Navigation = StackNavigator(
       screen: HomepageStack,
     },
     HotelsPackage: {
-      screen: class HotelsPackageWrapper extends React.Component<Props> {
-        goToHomepage = () => this.props.navigation.goBack();
-
-        render = () => {
-          const affiliate = String(Config.affiliate.bookingCom);
-          return (
-            <HotelsStandalonePackage
-              bookingComAffiliate={affiliate}
-              language="en"
-              currency="EUR" // Only EUR is now fully supported as PriceFilter can't handle anything but EUR
-              onBackClicked={this.goToHomepage}
-              dataSaverEnabled={false}
-            />
-          );
-        };
-      },
+      screen: HotelsPackageWrapper,
     },
   },
   {
-    initialRouteName: 'HotelsPackage', // FIXME!
+    initialRouteName: 'Homepage',
     navigationOptions: {
       header: null,
     },
