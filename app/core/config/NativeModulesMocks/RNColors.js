274668118fa872e00fc229c79be9723b8d71f40f
Trond Bergquist <trond_bergquist@hotmail.com>
2018-02-23 16:13:51 +0100

    Use native colors

---
+++ app/core/config/NativeModulesMocks/RNColors.js
@@ -0,0 +1,14 @@
+// @flow
+
+import { NativeModules } from 'react-native';
+
+NativeModules.RNColors = {
+  backgroundGray: '#EDEFF2',
+  brand: '#0097A9',
+  brandSecondary: '#0CB3C7',
+  buttercup: '#EB9D08',
+  sun: '#FBAD18',
+  textDark: '#30363D',
+  textLight: '#79818A',
+  textMedium: '#596066',
+};
---
+++ scripts/podInstall.js
@@ -0,0 +1,8 @@
+// @flow
+
+const path = require('path');
+const exec = require('child_process').execSync;
+
+const iosPath = path.join(__dirname, '..', 'ios');
+
+exec('pod install', { cwd: iosPath, stdio: 'inherit' });
