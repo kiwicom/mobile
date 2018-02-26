3181654662c7056b01f0322c4a08e89b9e0312c0
Trond Bergquist <trond_bergquist@hotmail.com>
2018-02-26 14:49:35 +0100

    Add build package.json to blacklist

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -14,6 +14,7 @@ const sharedBlacklist = [
   /node_modules[/\\]react[/\\]dist[/\\].*/,
   /website\/node_modules\/.*/,
   /heapCapture\/bundle\.js/,
+  /\.build[/\\].*/,
   // /.*\/__tests__\/.*/,
 ];
 
