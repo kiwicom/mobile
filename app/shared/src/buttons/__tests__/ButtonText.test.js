89e6af892cd9cb91d833cdff1d7e03c649daf704
Trond Bergquist <trond_bergquist@hotmail.com>
2018-03-05 12:19:06 +0100

    Uppercase button text on android

---
+++ app/shared/src/buttons/__tests__/ButtonText.test.js
@@ -0,0 +1,30 @@
+// @flow
+
+import * as React from 'react';
+import { Platform } from 'react-native';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+import ButtonText from '../ButtonText';
+
+let originalPlatform;
+let renderer;
+
+beforeEach(() => {
+  originalPlatform = Platform.OS;
+  renderer = new ShallowRenderer();
+});
+
+afterEach(() => (Platform.OS = originalPlatform));
+
+describe('ButtonText', () => {
+  it('should uppercase text on android', () => {
+    Platform.OS = 'android';
+    renderer.render(<ButtonText text="Button text" />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it('should not be uppercased on ios', () => {
+    Platform.OS = 'ios';
+    renderer.render(<ButtonText text="Button text" />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
+});
