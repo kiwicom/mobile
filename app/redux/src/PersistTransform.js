bfe957c20d970b584d7949d937b409d70feac3f9
Trond Bergquist <trond_bergquist@hotmail.com>
2018-03-16 14:07:27 +0100

    Add date transform to the Redux Persist
    All dates persisted in the store will be rehydrated to the valid Date
    object instead of just pure string.

---
+++ app/redux/src/PersistTransform.js
@@ -0,0 +1,36 @@
+// @flow
+
+import moment from 'moment';
+import { createTransform } from 'redux-persist';
+
+export const transformDateStringsToDate = (input: mixed) => {
+  if (typeof input === 'string' && moment(input).isValid()) {
+    return new Date(input);
+  }
+  return input;
+};
+
+/**
+ * Copies deeply nested objects like
+ * input = { a: 1, b: 2, c: { d: 3 } }
+ */
+export const copyObject = (input: Object) => {
+  let output = {};
+  Object.keys(input).forEach(key => {
+    if (typeof input[key] === 'object' && input[key] != null) {
+      output[key] = copyObject(input[key]);
+    } else {
+      output[key] = transformDateStringsToDate(input[key]);
+    }
+  });
+  return output;
+};
+
+const dateTransform = createTransform(
+  state => state,
+  state => {
+    return copyObject(state);
+  },
+);
+
+export default [dateTransform];
