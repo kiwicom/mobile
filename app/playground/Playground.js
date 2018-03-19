f23a12d9b9043e1f49acfc94bfaee99521735e16
Martin Zlámal <mrtnzlml@gmail.com>
2018-03-19 11:39:26 +0000

    Shared: use Playground Renderer for 'Text' component tests

--- app/playground/Playground.js
+++ app/playground/Playground.js
@@ -7,7 +7,7 @@ import { StyleSheet, Text } from '@kiwicom/react-native-app-shared';
 import PlaygroundRenderer from './PlaygroundRenderer';
 // Import component tests you want to show in the Playground here:
 // eslint-disable-next-line
-import _ from '../hotels/src/singleHotel/roomPicker/__tests__/RoomPicker.test';
+import _ from '../shared/src/__tests__/Text.test';
 
 const PlaygroundSection = props => (
   <View style={styles.section}>
