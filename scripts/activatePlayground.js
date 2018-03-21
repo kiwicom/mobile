efe5f2172270781987b0aa2ea7edd77c0f0750e2
Trond Bergquist <trond_bergquist@hotmail.com>
2018-03-21 09:59:01 +0100

    Fix datepicker button colors
    Add script to activate playground

---
+++ scripts/activatePlayground.js
@@ -0,0 +1,18 @@
+// @flow
+
+const fs = require('fs');
+const path = require('path');
+
+const indexFilePath = path.join(__dirname, '..', 'index.js');
+
+const indexFileContent = `// @flow
+
+import { AppRegistry } from 'react-native';
+
+// import App from './app/App';
+import App from './app/playground/Playground';
+
+AppRegistry.registerComponent('reactNativeApp', () => App);
+`;
+
+fs.writeFileSync(indexFilePath, indexFileContent);
