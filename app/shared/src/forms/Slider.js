5f1f2b76dfddecb1e8b33cceb5dc2caa2c43c602
Trond Bergquist <trond_bergquist@hotmail.com>
2018-03-26 09:26:03 +0200

    Show slider correctly when max and min values are equal

--- app/shared/src/forms/Slider.js
+++ app/shared/src/forms/Slider.js
@@ -32,8 +32,36 @@ export default class Slider extends React.Component<Props, State> {
     this.setState({ width: nativeEvent.layout.width });
   };
 
+  getMaxMinAndEnabled = () => {
+    const { max, min } = this.props;
+
+    if (
+      max === min &&
+      max === this.props.startValue &&
+      max === this.props.endValue
+    ) {
+      // If all values are equal, expand max and min values by one
+      // to make dot appear on the center and disable sliding.
+      return {
+        max: max + 1,
+        min: min - 1,
+        enabledOne: false,
+        enabledTwo: false,
+      };
+    }
+
+    return {
+      max,
+      min,
+      enabledOne: true,
+      enabledTwo: true,
+    };
+  };
+
   render() {
+    const { max, min, enabledOne, enabledTwo } = this.getMaxMinAndEnabled();
     const values = [this.props.startValue];
+
     if (this.props.endValue) {
       values.push(this.props.endValue);
     }
@@ -42,8 +70,8 @@ export default class Slider extends React.Component<Props, State> {
       <View onLayout={this.onLayout} style={styles.sliderWrapper}>
         <MultiSlider
           values={values}
-          min={this.props.min}
-          max={this.props.max}
+          min={min}
+          max={max}
           snapped={this.props.snapped}
           allowOverlap
           selectedStyle={styles.selected}
@@ -60,6 +88,8 @@ export default class Slider extends React.Component<Props, State> {
           pressedMarkerStyle={styles.marker}
           containerStyle={[styles.container, this.props.style]}
           onValuesChange={this.props.onChange}
+          enabledOne={enabledOne}
+          enabledTwo={enabledTwo}
         />
       </View>
     );
---
+++ app/shared/src/forms/__tests__/Slider.test.js
@@ -0,0 +1,40 @@
+// @flow
+
+import * as React from 'react';
+import { PlaygroundRenderer } from '@kiwicom/react-native-app-playground';
+
+import Slider from '../Slider';
+
+const onChange = () => {};
+
+describe('Slider', () => {
+  it('renders', () => {
+    PlaygroundRenderer.render(
+      <Slider
+        startValue={1}
+        endValue={50}
+        onChange={onChange}
+        min={1}
+        max={50}
+      />,
+    );
+  });
+
+  it('renders correctly with all values equal', () => {
+    PlaygroundRenderer.render(
+      <Slider
+        startValue={500}
+        endValue={500}
+        onChange={onChange}
+        min={500}
+        max={500}
+      />,
+    );
+  });
+
+  it('renders with endValue undefined', () => {
+    PlaygroundRenderer.render(
+      <Slider startValue={1} onChange={onChange} min={1} max={50} />,
+    );
+  });
+});
