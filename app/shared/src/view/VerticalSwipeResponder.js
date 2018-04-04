2387e97a3a5e58786395c85064ef65bb308c95ba
Trond Bergquist <trond_bergquist@hotmail.com>
2018-04-04 10:25:42 +0200

    Refactor unsafe_componentWillReceiveProps method

--- app/shared/src/view/VerticalSwipeResponder.js
+++ app/shared/src/view/VerticalSwipeResponder.js
@@ -69,9 +69,8 @@ export default class VerticalSwipeResponder extends React.Component<Props> {
     });
   }
 
-  UNSAFE_componentWillReceiveProps = (props: Props) => {
-    // TODO: Refactor, not sure how to do this in static component
-    this.swipeConfig = Object.assign(swipeConfig, props.config);
+  componentDidUpdate = () => {
+    this.swipeConfig = Object.assign(swipeConfig, this.props.config);
   };
 
   handleShouldSetPanResponder = (
