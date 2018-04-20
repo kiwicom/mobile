6d270bee80d5d62140173d4b8298974bac56da02
Trond Bergquist <trond_bergquist@hotmail.com>
2018-04-20 12:54:41 +0200

    Replace sync native methods with async methods

--- app/shared/src/currency/CurrencyFormatter.js
+++ app/shared/src/currency/CurrencyFormatter.js
@@ -2,6 +2,13 @@
 
 import { NativeModules } from 'react-native';
 
-export default function CurrencyFormatter(price: number, toCurrency: string) {
-  return NativeModules.RNCurrencyManager.formatAmount(price, toCurrency);
+export default async function CurrencyFormatter(
+  price: number,
+  toCurrency: string,
+) {
+  const formattedAmount = await NativeModules.RNCurrencyManager.formatAmountAsync(
+    price,
+    toCurrency,
+  );
+  return formattedAmount;
 }
