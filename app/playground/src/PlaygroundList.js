c84fa3bf4630b2c4c0507c761ccaeef188b46c7c
Trond Bergquist <trond_bergquist@hotmail.com>
2018-04-23 15:04:45 +0200

    Inject playground tests and start playground

--- app/playground/src/PlaygroundList.js
+++ app/playground/src/PlaygroundList.js
@@ -5,27 +5,9 @@ import { FlatList, View } from 'react-native';
 import type { NavigationType } from '@kiwicom/mobile-navigation';
 import { WithStorage } from '@kiwicom/mobile-shared';
 
-import '../../shared/src/forms/datePicker/__tests__/DatePicker.test.js';
-import '../../hotels/src/allHotels/__tests__/LoadMoreButton.test.js';
-import '../../hotels/src/allHotels/searchForm/locationPicker/__tests__/RecentSearches.test.js';
-import '../../hotels/src/allHotels/searchForm/locationPicker/__tests__/SuggestionList.test.js';
-import '../../hotels/src/filter/stars/__tests__/StarsFilter.test.js';
-import '../../hotels/src/filter/stars/__tests__/StarsPopup.test.js';
-import '../../hotels/src/gallery/__tests__/PhotosStripeHeader.test.js';
-import '../../hotels/src/singleHotel/bookNow/__tests__/BookNow.test.js';
-import '../../hotels/src/singleHotel/roomList/__tests__/RoomBadges.test.js';
-import '../../hotels/src/singleHotel/roomList/__tests__/RoomDescription.test.js';
-import '../../hotels/src/singleHotel/roomPicker/__tests__/RoomPicker.test.js';
-import '../../shared/src/__tests__/ReadMore.test.js';
-import '../../shared/src/__tests__/Text.test.js';
-import '../../shared/src/badge/__tests__/AdaptableBadge.test.js';
-import '../../shared/src/buttons/__tests__/Button.test.js';
-import '../../shared/src/buttons/__tests__/IncrementDecrementButtons.test.js';
-import '../../shared/src/cards/__tests__/SimpleCard.test.js';
-import '../../shared/src/forms/__tests__/Slider.test.js';
-import '../../shared/src/popup/__tests__/ButtonPopupPlayground.test.js';
 import PlaygroundListItem from './PlaygroundListItem';
 import PlaygroundRenderer from './PlaygroundRenderer';
+import './PlaygroundImports';
 
 type PlaygroundComponent = {|
   name: string,
--- scripts/activatePlayground.js
+++ scripts/activatePlayground.js
@@ -2,8 +2,19 @@
 
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+const execSync = require('child_process').execSync;
+const findSync = require('find-in-files').findSync;
 
 const indexFilePath = path.join(__dirname, '..', 'index.js');
+const playgroundImportsFilePath = path.join(
+  __dirname,
+  '..',
+  'app',
+  'playground',
+  'src',
+  'PlaygroundImports.js',
+);
 
 const indexFileContent = `// @flow
 
@@ -16,3 +27,27 @@ AppRegistry.registerComponent('reactNativeApp', () => App);
 `;
 
 fs.writeFileSync(indexFilePath, indexFileContent);
+
+// conviently enough, find sync returs a promise
+findSync('PlaygroundRenderer.render', 'app')
+  .then(files => {
+    return Object.keys(files);
+  })
+  .then(playgroundTestFiles => {
+    const playgroundImportsFileContent = [
+      '// @flow',
+      '',
+      playgroundTestFiles
+        .map(
+          // Replacing app from filepath with ../.. which is the path from playgroundList component to app folder
+          item => `import '${item.replace('app', '../..')}';`,
+        )
+        .join(os.EOL),
+      '',
+    ].join(os.EOL);
+
+    fs.writeFileSync(playgroundImportsFilePath, playgroundImportsFileContent);
+
+    // Can also be started on android with yarn playground android
+    execSync(`yarn ${process.argv[2] || 'ios'}`, { stdio: 'inherit' });
+  });
