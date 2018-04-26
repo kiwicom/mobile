ef179afb410f548ccdd4ebeae4a159b7eb1a4368
Trond Bergquist <trond_bergquist@hotmail.com>
2018-04-26 14:59:51 +0200

    Refactor all hotels
    Use cityId or coordinates if passed, or use device location.
    If user does not give permission, show message to ask user to search for city

--- app/hotels/src/map/allHotels/AllHotelsMap.js
+++ app/hotels/src/map/allHotels/AllHotelsMap.js
@@ -8,7 +8,10 @@ import {
   StyleSheet,
   AdaptableLayout,
   AppStateChange,
+  GeolocationContext,
+  IconLoading,
 } from '@kiwicom/mobile-shared';
+import { Translation } from '@kiwicom/mobile-localization';
 
 import MapScreen from './MapScreen';
 import FilterStripe from '../../filter/FilterStripe';
@@ -17,10 +20,7 @@ import type {
   SearchParams,
   OnChangeSearchParams,
 } from '../../allHotels/searchForm/SearchParametersType';
-import type {
-  FilterParams,
-  OnChangeFilterParams,
-} from '../../filter/FilterParametersType';
+import type { FilterParams } from '../../filter/FilterParametersType';
 import { handleOpenSingleHotel } from '../../singleHotel';
 import type { AvailableHotelSearchInput } from '../../singleHotel/AvailableHotelSearchInput';
 import { sanitizeHotelFacilities } from '../../GraphQLSanitizers';
@@ -36,6 +36,12 @@ const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
   },
+  noSearchContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    paddingHorizontal: 10,
+  },
 });
 
 type PropsWithContext = {
@@ -43,7 +49,9 @@ type PropsWithContext = {
   search: SearchParams,
   cityId: string | null,
   filter: FilterParams,
-  onFilterChange: OnChangeFilterParams => void,
+  lat: number | null,
+  lng: number | null,
+  canGetUserLocation: boolean,
   onSearchChange: OnChangeSearchParams => void,
 };
 
@@ -67,25 +75,32 @@ class AllHotelsMap extends React.Component<PropsWithContext> {
     );
   };
 
+  getCoordinates = () => {
+    const { coordinates, lat, lng } = this.props;
+
+    if (coordinates) {
+      return coordinates;
+    }
+    if (lat !== null && lng !== null) {
+      return {
+        latitude: lat,
+        longitude: lng,
+      };
+    }
+    return null;
+  };
+
   renderInnerComponent = (props: AllHotelsMapQueryResponse) => (
     <MapScreen
       data={props.allAvailableHotels}
-      filter={this.props.filter}
-      onFilterChange={this.props.onFilterChange}
       onOpenSingleHotel={this.handleOpenSingleHotel}
     />
   );
 
   render = () => {
-    const {
-      cityId,
-      search,
-      filter,
-      onFilterChange,
-      currency,
-      coordinates,
-    } = this.props;
-
+    const { cityId, search, currency, filter } = this.props;
+    const coordinates = this.getCoordinates();
+    const canSearch = cityId || coordinates;
     return (
       <AppStateChange
         states={['active']}
@@ -93,42 +108,46 @@ class AllHotelsMap extends React.Component<PropsWithContext> {
       >
         <View style={styles.container}>
           <AdaptableLayout
-            renderOnNarrow={
-              <FilterStripe
-                filter={filter}
-                onChange={onFilterChange}
-                currency={currency}
-              />
-            }
+            renderOnNarrow={<FilterStripe currency={currency} />}
           />
-          <PublicApiRenderer
-            query={graphql`
-              query AllHotelsMapQuery(
-                $search: HotelsSearchInput!
-                $filter: HotelsFilterInput
-                $options: AvailableHotelOptionsInput
-              ) {
-                allAvailableHotels(
-                  search: $search
-                  filter: $filter
-                  options: $options
+          {canSearch ? (
+            <PublicApiRenderer
+              query={graphql`
+                query AllHotelsMapQuery(
+                  $search: HotelsSearchInput!
+                  $filter: HotelsFilterInput
+                  $options: AvailableHotelOptionsInput
                 ) {
-                  ...MapScreen
+                  allAvailableHotels(
+                    search: $search
+                    filter: $filter
+                    options: $options
+                  ) {
+                    ...MapScreen
+                  }
                 }
-              }
-            `}
-            variables={{
-              search: getSearchQueryParams(search, coordinates, cityId),
-              filter: {
-                ...filter,
-                hotelFacilities: sanitizeHotelFacilities(
-                  filter.hotelFacilities,
-                ),
-              },
-              options: { currency },
-            }}
-            render={this.renderInnerComponent}
-          />
+              `}
+              variables={{
+                search: getSearchQueryParams(search, coordinates, cityId),
+                filter: {
+                  ...filter,
+                  hotelFacilities: sanitizeHotelFacilities(
+                    filter.hotelFacilities,
+                  ),
+                },
+                options: { currency },
+              }}
+              render={this.renderInnerComponent}
+            />
+          ) : (
+            <View style={styles.noSearchContainer}>
+              {!this.props.canGetUserLocation ? (
+                <Translation id="hotels_search.all_hotels_map.allow_localization_or_search" />
+              ) : (
+                <IconLoading />
+              )}
+            </View>
+          )}
         </View>
       </AppStateChange>
     );
@@ -137,8 +156,8 @@ class AllHotelsMap extends React.Component<PropsWithContext> {
 
 type Props = {|
   currency: string,
-  onGoToSingleHotel: (searchParams: AvailableHotelSearchInput) => void,
   coordinates: Coordinates | null,
+  onGoToSingleHotel: (searchParams: AvailableHotelSearchInput) => void,
 |};
 
 export default function AllHotelsMapWithContext(props: Props) {
@@ -146,15 +165,21 @@ export default function AllHotelsMapWithContext(props: Props) {
     <HotelsSearchContext.Consumer>
       {({ cityId, searchParams, actions: searchActions }) => (
         <HotelsFilterContext.Consumer>
-          {({ filterParams, actions: filterActions }) => (
-            <AllHotelsMap
-              {...props}
-              cityId={cityId}
-              search={searchParams}
-              filter={filterParams}
-              onFilterChange={filterActions.setFilter}
-              onSearchChange={searchActions.setSearch}
-            />
+          {({ filterParams }) => (
+            <GeolocationContext.Consumer>
+              {({ lat, lng, canGetUserLocation }) => (
+                <AllHotelsMap
+                  {...props}
+                  cityId={cityId}
+                  search={searchParams}
+                  filter={filterParams}
+                  onSearchChange={searchActions.setSearch}
+                  lat={lat}
+                  lng={lng}
+                  canGetUserLocation={canGetUserLocation}
+                />
+              )}
+            </GeolocationContext.Consumer>
           )}
         </HotelsFilterContext.Consumer>
       )}
