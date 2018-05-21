6af7c1aa679dede4a8c563e8feec632cdffe1fe8
Mike Grabowski <grabbou@gmail.com>
2018-05-21 11:46:54 +0200

    Add placeholder

---
+++ scripts/releaseFramework.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#####
+# Script for releasing a new version of the framework
+# and putting it on Github as a release
+####
+
+# Build iOS FAT framework
+./scripts/buildIOSFramework.sh
+
+# Tag a new release
+
+# Make Github release
+
+# Upload release artifacts
