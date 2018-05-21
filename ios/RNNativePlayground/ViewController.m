17b79f1587e8e1db8ff5cac665690d53811fc0b7
Mike Grabowski <grabbou@gmail.com>
2018-05-21 08:53:08 +0200

    Implement bridge cache

--- ios/RNNativePlayground/ViewController.m
+++ ios/RNNativePlayground/ViewController.m
@@ -7,6 +7,21 @@
 
 @implementation ViewController
 
+- (instancetype)initWithCoder:(NSCoder *)coder {
+  self = [super initWithCoder:coder];
+  if (self) {
+    /**
+     * In order to create bridge upfront, either create an instance of RNKiwiViewController
+     * ahead of time (here) and store as an instance property, or call this method instead.
+     *
+     * RNKiwiViewController does exactly the same thing for you behind the scenes if you use
+     * the former approach.
+     */
+    [[RNKiwiSharedBridge sharedInstance] initBridgeWithOptions:self];
+  }
+  return self;
+}
+
 - (IBAction)showHotelsView:(id)sender {
   RNKiwiViewController *vc = [[RNKiwiViewController alloc] initWithOptions:self];
   [vc setCurrencyFormatter:self];
@@ -29,6 +44,10 @@
 
 - (NSString *)moduleName {
   return @"KiwiHotels";
+}
+
+- (NSURL *)jsCodeLocation {
+  return RNKiwiConstants.hotelsBundle;
 }
 
 # pragma mark - RNKiwiCurrencyManager
