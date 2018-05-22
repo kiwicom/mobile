6ecd7418c485514eb67f404bc947207c8469b749
Martin Zlámal <mrtnzlml@gmail.com>
2018-05-22 18:01:56 +0200

    Fix Flow 'StylePropType'
    It returned 'any' because of changed Relay internal structure. Don't be
    scared by the weird names - I didn't find a better way how to do it. The
     good thing is that it will handle much more. For example it will not
     allow you to do this because it's invalid:
    
    ```
    {
      fontWeight: 500,
    }
    ```

--- app/shared/types/Styles.js
+++ app/shared/types/Styles.js
@@ -4,6 +4,23 @@
  * Styles are only reexported here to avoid importing internal RN libraries
  * everywhere (types are not exported directly).
  */
-import type { StyleProp as NativeStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
-export type StylePropType = NativeStyleProp;
+import {
+  type ____DangerouslyImpreciseStyleProp_Internal,
+  type DangerouslyImpreciseStyle,
+} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+
+export type StyleObjectType = {
+  +[key: string]: $Shape<DangerouslyImpreciseStyle>,
+};
+
+export type PlatformStyleObjectType = {
+  +[key: string]: $Shape<
+    DangerouslyImpreciseStyle & {
+      android: DangerouslyImpreciseStyle,
+      ios: DangerouslyImpreciseStyle,
+    },
+  >,
+};
+
+export type StylePropType = ____DangerouslyImpreciseStyleProp_Internal;
