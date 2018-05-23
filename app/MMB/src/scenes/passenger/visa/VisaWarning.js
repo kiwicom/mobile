f110ecb4ccfac913e207e1e6ff3264f5b0c1c957
Trond Bergquist <trond_bergquist@hotmail.com>
2018-05-23 12:27:29 +0200

    MMB: Add visa warning

---
+++ app/MMB/src/scenes/passenger/visa/VisaWarning.js
@@ -0,0 +1,37 @@
+// @flow
+
+import * as React from 'react';
+import { Translation } from '@kiwicom/mobile-localization';
+import { StyleSheet, Text } from '@kiwicom/mobile-shared';
+
+import Alert from '../../../components/alert/Alert';
+
+type Props = {|
+  countries: string[],
+|};
+
+export default function VisaWarning(props: Props) {
+  if (props.countries.length === 0) {
+    return null;
+  }
+  return (
+    <Alert
+      type="warning"
+      title={<Translation id="mmb.visa_state.visa_may_be_required" />}
+    >
+      {props.countries.map(country => (
+        <Text key={country} style={styles.country}>
+          <Translation passThrough={country} />
+        </Text>
+      ))}
+    </Alert>
+  );
+}
+
+const styles = StyleSheet.create({
+  country: {
+    fontSize: 12,
+    color: '#eb9d08',
+    fontWeight: '600',
+  },
+});
