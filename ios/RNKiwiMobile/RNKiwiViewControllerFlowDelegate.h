22198ba28537be2af0ebc6c07d292ce989ecc943
lukewalczak <lukasz.walczak@callstack.io>
2018-05-24 11:53:14 +0200

    Connect swiping solution into rn playground

--- ios/RNKiwiMobile/RNKiwiViewControllerFlowDelegate.h
+++ ios/RNKiwiMobile/RNKiwiViewControllerFlowDelegate.h
@@ -1,7 +1,7 @@
 @class RNKiwiViewController;
 
 @protocol RNKiwiViewControllerFlowDelegate <NSObject>
-
+@optional
 - (void)RNKiwiViewControllerDidFinish:(nonnull RNKiwiViewController *)viewController;
 - (void)RNKiwiViewControllerDidStartControllingGestures:(nonnull RNKiwiViewController *)viewController;
 - (void)RNKiwiViewControllerDidStopControllingGestures:(nonnull RNKiwiViewController *)viewController;
