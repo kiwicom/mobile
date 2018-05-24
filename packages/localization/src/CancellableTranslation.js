6ac308382142aea8f4d1c2b2ea81383849fa325b
Trond Bergquist <trond_bergquist@hotmail.com>
2018-05-24 10:17:03 +0200

    Localization: Make translation promise cancellable

---
+++ packages/localization/src/CancellableTranslation.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { NativeModules } from 'react-native';
+
+export type TranslationPromise = {|
+  cancel: () => void,
+  promise: Promise<string>,
+|};
+
+export function cancellableTranslation(nativeKey: string): TranslationPromise {
+  let resolve;
+  let reject;
+
+  const promise = new Promise((originalResolve, originalReject) => {
+    resolve = originalResolve;
+    reject = originalReject;
+  });
+
+  NativeModules.RNTranslationManager.translateAsync(nativeKey)
+    .then(resolve)
+    .then(reject);
+
+  return {
+    promise,
+    cancel: () => {
+      reject('cancelled');
+    },
+  };
+}
