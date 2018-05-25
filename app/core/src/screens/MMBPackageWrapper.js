477043667d167a3e678049b2f0b26ccd86163848
Trond Bergquist <trond_bergquist@hotmail.com>
2018-05-25 09:55:23 +0200

    Core: Add login through profile tab

--- app/core/src/screens/MMBPackageWrapper.js
+++ app/core/src/screens/MMBPackageWrapper.js
@@ -1,16 +1,70 @@
 // @flow
 
 import * as React from 'react';
+import { AsyncStorage, View } from 'react-native';
 import { ManageMyBookingPackage } from '@kiwicom/mobile-manage-my-booking';
 import { type NavigationType } from '@kiwicom/mobile-navigation';
-import { MMB_TOKEN } from 'react-native-dotenv';
+import { Translation } from '@kiwicom/mobile-localization';
+import { StyleSheet } from '@kiwicom/mobile-shared';
 
 type Props = {|
   navigation: NavigationType,
 |};
 
-export default class MMBPackageWrapper extends React.Component<Props> {
-  render = () => (
-    <ManageMyBookingPackage currency="EUR" accessToken={MMB_TOKEN} />
-  );
+export default class MMBPackageWrapper extends React.Component<
+  Props,
+  {| token: string | null |},
+> {
+  state = {
+    token: '',
+  };
+
+  static navigationOptions = ({
+    navigation,
+  }: {
+    navigation: NavigationType,
+  }) => {
+    return {
+      tabBarOnPress: ({
+        scene,
+        jumpToIndex,
+      }: {
+        scene: {| index: number |},
+        jumpToIndex: (index: number) => void,
+      }) => {
+        jumpToIndex(scene.index);
+        navigation.state.params.fetchToken();
+      },
+    };
+  };
+
+  componentDidMount = () => {
+    this.fetchToken();
+    this.props.navigation.setParams({ fetchToken: this.fetchToken });
+  };
+
+  fetchToken = async () => {
+    const token = await AsyncStorage.getItem('mobile:MMB-Token');
+    this.setState({ token });
+  };
+
+  render = () => {
+    if (!this.state.token) {
+      return (
+        <View style={styles.loginMessageContainer}>
+          <Translation passThrough="You are not logged in, go to profile and log in" />
+        </View>
+      );
+    }
+
+    return (
+      <ManageMyBookingPackage currency="EUR" accessToken={this.state.token} />
+    );
+  };
 }
+
+const styles = StyleSheet.create({
+  loginMessageContainer: {
+    marginTop: 60,
+  },
+});
