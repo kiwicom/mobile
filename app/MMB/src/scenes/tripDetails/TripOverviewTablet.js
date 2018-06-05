5b90c8d7417b6013b1a6f82a74c9a888427ed704
Martin Zlámal <mrtnzlml@gmail.com>
2018-06-05 17:04:22 +0200

    MMB: add basic trip overview layout

---
+++ app/MMB/src/scenes/tripDetails/TripOverviewTablet.js
@@ -0,0 +1,8 @@
+// @flow strict
+
+import * as React from 'react';
+import { Translation } from '@kiwicom/mobile-localization';
+
+export default function TripOverviewTablet() {
+  return <Translation passThrough="Big trip overview on the tablet" />;
+}
