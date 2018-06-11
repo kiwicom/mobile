5a384191672aa401bf344629d5793edb4cd6172e
ferrannp <fnp.developer@gmail.com>
2018-06-11 15:46:46 +0200

    fix: iOS release script - print output + create github tag after framework is built

--- scripts/releaseFramework.js
+++ scripts/releaseFramework.js
@@ -1,5 +1,7 @@
 // @flow strict
 
+/* eslint-disable no-console */
+
 /**
  * Script for releasing a new version of the framework
  * and putting it on Github as a release
@@ -7,7 +9,6 @@
  * You need to have GITHUB_TOKEN present in your environment with correct
  * write/read permissions (public_repo)
  */
-
 const fs = require('fs');
 const child_process = require('child_process');
 const fetch = require('node-fetch');
@@ -19,67 +20,79 @@ let version = child_process
   .trim();
 
 (async () => {
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-  if (!GITHUB_TOKEN) {
-    throw Error("Variable 'process.env.GITHUB_TOKEN' is not set.");
+  try {
+    const githubToken = process.env.GITHUB_TOKEN;
+    if (githubToken == null) throw new Error('Missing GITHUB_TOKEN');
+
+    console.log(`Start building FAT framework...`);
+
+    // Build FAT framework
+    child_process.execSync('scripts/buildIOSFramework.sh', {
+      stdio: 'inherit',
+    });
+
+    // Last line is empty
+    const fatFramework = 'ios/build/universal/RNKiwiMobile.framework';
+    const frameworkFolder = path.dirname(fatFramework);
+    const zippedFatFramework = `RNKiwiMobile.framework.zip`;
+
+    console.log(`FAT framework was correctly zipped.`);
+
+    /**
+     * Zip file to its destination to prepare for upload
+     */
+    child_process.execSync(
+      `cd ${frameworkFolder} && zip -r ${zippedFatFramework} .`,
+    );
+
+    /**
+     * Create Github release that points to the latest commit
+     */
+    const release = await (await fetch(
+      `https://api.github.com/repos/kiwicom/mobile/releases?access_token=${githubToken}`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          tag_name: version,
+          target_commitish: 'master',
+          name: version,
+          draft: false,
+          prerelease: false,
+        }),
+      },
+    )).json();
+
+    console.log(`GitHub tag ${version} was created.`);
+
+    /**
+     * Github API doesn't support sending files with unknown `Content-Lenght`. That means we
+     * cannot send a stream.
+     */
+    const body = fs.readFileSync(
+      path.join(frameworkFolder, zippedFatFramework),
+    );
+    await fetch(
+      `https://uploads.github.com/repos/kiwicom/mobile/releases/${
+        release.id
+      }/assets?name=${zippedFatFramework}&access_token=${githubToken}`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/octet-stream',
+          'Content-Length': body.length,
+        },
+        body,
+      },
+    );
+
+    console.log(`Release ${version} successfully created.`);
+  } catch (e) {
+    console.log(`releaseFramework failed.`, e.message);
+    process.exit(1);
   }
-
-  // create Github release that points to the latest commit
-  const release = await (await fetch(
-    `https://api.github.com/repos/kiwicom/mobile/releases?access_token=${GITHUB_TOKEN}`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        tag_name: version,
-        target_commitish: 'master',
-        name: version,
-        draft: false,
-        prerelease: false,
-      }),
-    },
-  )).json();
-
-  // Build FAT framework
-  const buildOutput = child_process
-    .execSync('scripts/buildIOSFramework.sh')
-    .toString()
-    .split('\n');
-
-  // Last line is empty
-  const fatFramework = buildOutput[buildOutput.length - 2].trim();
-  const frameworkFolder = path.dirname(fatFramework);
-  const zippedFatFramework = `${fatFramework}.zip`;
-
-  /**
-   * Zip file to its destination to preare for upload
-   */
-  child_process.execSync(
-    `cd ${frameworkFolder} && zip -r ${zippedFatFramework} .`,
-  );
-
-  /**
-   * Github API doesn't support sending files with unknown `Content-Lenght`. That means we
-   * cannot send a stream.
-   */
-  const body = fs.readFileSync(zippedFatFramework);
-  await fetch(
-    `https://uploads.github.com/repos/kiwicom/mobile/releases/${
-      release.id
-    }/assets?name=${path.basename(
-      zippedFatFramework,
-    )}&access_token=${GITHUB_TOKEN}`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/octet-stream',
-        'Content-Length': body.length,
-      },
-      body,
-    },
-  );
 })();
