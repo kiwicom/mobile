faf0bacd3e3c5c3e840e251f4a3d9fb37316444d
Martin Zlámal <mrtnzlml@gmail.com>
2018-06-21 14:52:34 +0200

    MMB insurance: prepare missing screens (no design yet)

---
+++ app/MMB/src/scenes/tripServices/insurance/RefundScene.js
@@ -0,0 +1,8 @@
+// @flow
+
+import * as React from 'react';
+import { Translation } from '@kiwicom/mobile-localization';
+
+export default function RefundScene() {
+  return <Translation passThrough="TODO refund" />;
+}
