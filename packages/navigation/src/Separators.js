5b765382ffe0f78d49381cf42ab3899f87744af7
Martin Zlámal <mrtnzlml@gmail.com>
2018-06-25 11:39:02 +0200

    MMB insurance: design order summary overview

--- packages/navigation/src/Separators.js
+++ packages/navigation/src/Separators.js
@@ -4,6 +4,10 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { Color, StyleSheet } from '@kiwicom/mobile-shared';
 
+type SeparatorFullWidthType = {|
+  +color?: string,
+|};
+
 /**
  * Fill width grey line:
  *
@@ -11,28 +15,35 @@ import { Color, StyleSheet } from '@kiwicom/mobile-shared';
  * |---------------------------|
  * |                           |
  */
-export function SeparatorFullWidth() {
-  return <View style={styleSheet.separator} />;
+export function SeparatorFullWidth(props: SeparatorFullWidthType) {
+  let style = {};
+  if (props.color) {
+    style = {
+      backgroundColor: props.color,
+    };
+  }
+  return <View style={[styleSheet.separator, style]} />;
 }
 
+type SeparatorTrimmedType = {|
+  +gapSizeStart: number,
+  +gapSizeEnd?: number,
+  +color?: string,
+|};
+
 /**
- * Grey line but not starting from the very beginning:
+ * Grey line but not starting from the very beginning (or the end):
  *
  * |                           |
  * |     ----------------------|
  * |                           |
  */
-export function SeparatorTrimmed({
-  gapSizeStart,
-  gapSizeEnd,
-}: {|
-  gapSizeStart: number,
-  gapSizeEnd?: number,
-|}) {
+export function SeparatorTrimmed(props: SeparatorTrimmedType) {
+  const { gapSizeStart, gapSizeEnd, color } = props;
   return (
     <View style={styleSheet.row}>
       <View style={[styleSheet.gap, { width: gapSizeStart }]} />
-      <SeparatorFullWidth />
+      <SeparatorFullWidth color={color} />
       <View style={[styleSheet.gap, { width: gapSizeEnd }]} />
     </View>
   );
