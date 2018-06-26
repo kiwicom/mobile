457e485c1a886e4348145c491465572e460db2cc
Trond Bergquist <trond_bergquist@hotmail.com>
2018-06-26 11:10:26 +0200

    Hotels: Filter stripe refactor + fix scroll to start

--- app/hotels/src/filter/freeCancellation/FreeCancellationFilter.js
+++ app/hotels/src/filter/freeCancellation/FreeCancellationFilter.js
@@ -12,15 +12,13 @@ type Props = {|
 |};
 
 export default class FreeCancellationFilter extends React.Component<Props> {
-  static isActive = (isActive: boolean) => isActive;
-
   onChange = () =>
     this.props.onChange({ freeCancellation: !this.props.isActive });
 
   render = () => (
     <FilterButton
       title={<Translation id="hotels_search.filter.free_cancellation" />}
-      isActive={this.constructor.isActive(this.props.isActive)}
+      isActive={this.props.isActive}
       onPress={this.onChange}
     />
   );
