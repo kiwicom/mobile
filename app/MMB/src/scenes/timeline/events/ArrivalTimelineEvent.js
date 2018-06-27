a554b8593c2235300c852135c8a0cc6b51505778
Robin Cussol <robin@loki.local>
2018-06-27 16:49:03 +0200

    Displaying location below date possible; highlighted date and location for AirportArrival and Arrival events

--- app/MMB/src/scenes/timeline/events/ArrivalTimelineEvent.js
+++ app/MMB/src/scenes/timeline/events/ArrivalTimelineEvent.js
@@ -9,6 +9,7 @@ import { TextIcon } from '@kiwicom/mobile-shared';
 import type { ArrivalTimelineEvent as ArrivalTimelineEventType } from './__generated__/ArrivalTimelineEvent.graphql';
 import TimelineEvent from '../TimelineEvent';
 import TimelineEventIcon from '../TimelineEventIcon';
+import TimelineEventDateLocationContext from '../../../context/TimelineEventDateLocationContext';
 
 type Props = {|
   +data: ArrivalTimelineEventType,
@@ -17,22 +18,25 @@ type Props = {|
 const ArrivalTimelineEvent = (props: Props) => {
   const timestamp = idx(props, _ => _.data.timestamp);
   const airport = idx(props, _ => _.data.location.airport.city.name);
-  const code = idx(props, _ => _.data.location.airport.locationId);
+  const code = idx(props, _ => _.data.location.airport.locationId) || '';
 
   return (
-    <TimelineEvent
-      timestamp={timestamp}
-      iconVertLines={<TimelineEventIcon icon={<TextIcon code="%" />} />}
-      mainContent={
-        <Translation
-          id="mmb.booking_timeline.event.arrival.title"
-          values={{
-            airport,
-            code,
-          }}
-        />
-      }
-    />
+    <TimelineEventDateLocationContext.Provider value={{ highlightText: true }}>
+      <TimelineEvent
+        timestamp={timestamp}
+        place={code}
+        iconVertLines={<TimelineEventIcon icon={<TextIcon code="%" />} />}
+        mainContent={
+          <Translation
+            id="mmb.booking_timeline.event.arrival.title"
+            values={{
+              airport,
+              code,
+            }}
+          />
+        }
+      />
+    </TimelineEventDateLocationContext.Provider>
   );
 };
 
