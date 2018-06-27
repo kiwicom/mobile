8db3d657b10e8b47c5ac261194cc3784ec116e4b
ferrannp <fnp.developer@gmail.com>
2018-06-27 19:55:52 +0200

    Android: add react-native package building to android snapshots script

--- scripts/buildAndroidSnapshots.js
+++ scripts/buildAndroidSnapshots.js
@@ -11,8 +11,11 @@ const exec = (command, options) =>
     ...options,
   });
 
-const DEFAULT_URL =
+const SKYPICKER_URL =
   'http://trinerdis.cz:8000/repository/snapshots/com/trinerdis/skypicker/';
+
+const FACEBOOK_URL =
+  'http://trinerdis.cz:8000/repository/snapshots/com/facebook/react/';
 
 if (!process.env.ANDROID_DEPLOYMENT_PASSWORD) {
   throw Error('You need to pass ANDROID_DEPLOYMENT_PASSWORD.');
@@ -29,41 +32,42 @@ const getDependencyVersion = packageName => {
 // Get rnkiwimobile version
 const RNKiwiMobileVersion = buildPackage.version;
 
-const deployDependency = packageName => {
+const deployDependency = (
+  packageName: string,
+  url: string,
+  extension: string = '',
+) => {
   const version = getDependencyVersion(packageName);
 
   return new Promise((resolve, reject) =>
-    urlExists(
-      `${DEFAULT_URL}${packageName}/${version}-SNAPSHOT/`,
-      (err, exists) => {
-        if (err) {
+    urlExists(`${url}${packageName}/${version}${extension}/`, (err, exists) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (!exists) {
+        console.log(`Deploying ${packageName}/${version}${extension}`);
+        try {
+          exec(
+            `cd RNAndroidPlayground/${packageName} && ANDROID_DEPLOYMENT_PASSWORD=${
+              // $FlowFixMe we already checked in the top that is defined
+              process.env.ANDROID_DEPLOYMENT_PASSWORD
+            } ../gradlew --no-daemon uploadTrinerdis -Pversion=${version}`,
+          );
+        } catch (err) {
           reject(err);
           return;
         }
-        if (!exists) {
-          console.log(`Deploying ${packageName}/${version}-SNAPSHOT`);
-          try {
-            exec(
-              `cd RNAndroidPlayground/${packageName} && ANDROID_DEPLOYMENT_PASSWORD=${
-                // $FlowFixMe we already checked in the top that is defined
-                process.env.ANDROID_DEPLOYMENT_PASSWORD
-              } ../gradlew --no-daemon uploadTrinerdis -Pversion=${version}`,
-            );
-          } catch (err) {
-            reject(err);
-            return;
-          }
-          console.log(
-            `${packageName}/${version}-SNAPSHOT was successfully deployed.`,
-          );
-        } else {
-          console.log(
-            `Skipping ${packageName}/${version}-SNAPSHOT (already deployed).`,
-          );
-        }
-        resolve();
-      },
-    ),
+        console.log(
+          `${packageName}/${version}${extension} was successfully deployed.`,
+        );
+      } else {
+        console.log(
+          `Skipping ${packageName}/${version}${extension} (already deployed).`,
+        );
+      }
+      resolve();
+    }),
   );
 };
 
@@ -90,9 +94,10 @@ const deployLibrary = (packageName, version) => {
 (() => {
   console.log(`Start building Android SNAPSHOT(s)...`);
   Promise.all([
-    deployDependency('react-native-maps'),
-    deployDependency('react-native-vector-icons'),
-    deployDependency('react-native-tooltips'),
+    deployDependency('react-native', FACEBOOK_URL),
+    deployDependency('react-native-maps', SKYPICKER_URL, '-SNAPSHOT'),
+    deployDependency('react-native-vector-icons', SKYPICKER_URL, '-SNAPSHOT'),
+    deployDependency('react-native-tooltips', SKYPICKER_URL, '-SNAPSHOT'),
   ])
     .then(() => {
       // Main package to publish: rnkiwimobile
