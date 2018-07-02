7adf4b5b9299a585999e5fa6ce0eecc21fb52fb0
Trond Bergquist <trond_bergquist@hotmail.com>
2018-07-02 17:04:41 +0200

    MMB: Add return outbound flights to boarding passes

---
+++ app/MMB/src/scenes/tickets/boardingPasses/FlightFromTo.js
@@ -0,0 +1,96 @@
+// @flow strict
+
+import * as React from 'react';
+import { View } from 'react-native';
+import { graphql, createFragmentContainer } from '@kiwicom/mobile-relay';
+import { Translation, DateFormatter } from '@kiwicom/mobile-localization';
+import { TextIcon, StyleSheet, Color, Text } from '@kiwicom/mobile-shared';
+import idx from 'idx';
+
+import type { FlightFromTo as RouteStopType } from './__generated__/FlightFromTo.graphql';
+
+type Props = {|
+  +data: RouteStopType,
+|};
+
+const FlightFromTo = (props: Props) => {
+  const date = idx(props.data, _ => _.departure.localTime);
+  const shortDate = date
+    ? DateFormatter(new Date(date)).formatToShortDate()
+    : '';
+  const time = date ? DateFormatter(new Date(date)).formatToTime() : '';
+  return (
+    <View style={styles.row}>
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateText}>
+          <Translation passThrough={shortDate} />
+        </Text>
+        <Text style={styles.dateText}>
+          <Translation passThrough={time} />
+        </Text>
+      </View>
+      <View style={[styles.row, styles.cityContainer]}>
+        <Text style={styles.cityText}>
+          <Translation
+            passThrough={idx(props.data, _ => _.departure.airport.city.name)}
+          />
+        </Text>
+        <TextIcon code="&#xe099;" style={styles.icon} />
+        <Text style={styles.cityText}>
+          <Translation
+            passThrough={idx(props.data, _ => _.arrival.airport.city.name)}
+          />
+        </Text>
+      </View>
+    </View>
+  );
+};
+export default createFragmentContainer(
+  FlightFromTo,
+  graphql`
+    fragment FlightFromTo on Leg {
+      departure {
+        localTime
+        airport {
+          city {
+            name
+          }
+        }
+      }
+      arrival {
+        airport {
+          city {
+            name
+          }
+        }
+      }
+    }
+  `,
+);
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  dateText: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: Color.textLight,
+  },
+  dateContainer: {
+    marginEnd: 10,
+  },
+  cityContainer: {
+    alignSelf: 'flex-start',
+  },
+  icon: {
+    color: Color.textLight,
+    fontSize: 8,
+    alignSelf: 'center',
+    marginHorizontal: 8,
+  },
+  cityText: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
---
+++ app/MMB/src/scenes/tickets/boardingPasses/__generated__/FlightFromTo.graphql.js
@@ -0,0 +1,107 @@
+/**
+ * @flow
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteFragment } from 'relay-runtime';
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type FlightFromTo$ref: FragmentReference;
+export type FlightFromTo = {|
+  +departure: ?{|
+    +localTime: ?any,
+    +airport: ?{|
+      +city: ?{|
+        +name: ?string
+      |}
+    |},
+  |},
+  +arrival: ?{|
+    +airport: ?{|
+      +city: ?{|
+        +name: ?string
+      |}
+    |}
+  |},
+  +$refType: FlightFromTo$ref,
+|};
+*/
+
+
+const node/*: ConcreteFragment*/ = (function(){
+var v0 = {
+  "kind": "LinkedField",
+  "alias": null,
+  "name": "airport",
+  "storageKey": null,
+  "args": null,
+  "concreteType": "Location",
+  "plural": false,
+  "selections": [
+    {
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "city",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "LocationArea",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "ScalarField",
+          "alias": null,
+          "name": "name",
+          "args": null,
+          "storageKey": null
+        }
+      ]
+    }
+  ]
+};
+return {
+  "kind": "Fragment",
+  "name": "FlightFromTo",
+  "type": "Leg",
+  "metadata": null,
+  "argumentDefinitions": [],
+  "selections": [
+    {
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "departure",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "RouteStop",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "ScalarField",
+          "alias": null,
+          "name": "localTime",
+          "args": null,
+          "storageKey": null
+        },
+        v0
+      ]
+    },
+    {
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "arrival",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "RouteStop",
+      "plural": false,
+      "selections": [
+        v0
+      ]
+    }
+  ]
+};
+})();
+// prettier-ignore
+(node/*: any*/).hash = 'b9c0c09e147be2363082ac0c9cbc77e9';
+module.exports = node;
