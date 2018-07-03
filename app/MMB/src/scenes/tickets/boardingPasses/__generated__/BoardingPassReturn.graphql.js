4590545c9bfcd4f1aa74e302d7fe3079a01bbb7d
Trond Bergquist <trond_bergquist@hotmail.com>
2018-07-03 09:23:35 +0200

    MMB: Add inbound flight segments to boarding passes

--- app/MMB/src/scenes/tickets/boardingPasses/__generated__/BoardingPassReturn.graphql.js
+++ app/MMB/src/scenes/tickets/boardingPasses/__generated__/BoardingPassReturn.graphql.js
@@ -8,17 +8,30 @@
 
 /*::
 import type { ConcreteFragment } from 'relay-runtime';
-type OutboundFlights$ref = any;
+type FlightSegments$ref = any;
 import type { FragmentReference } from "relay-runtime";
 declare export opaque type BoardingPassReturn$ref: FragmentReference;
 export type BoardingPassReturn = {|
-  +$fragmentRefs: OutboundFlights$ref,
+  +outbound: ?{|
+    +$fragmentRefs: FlightSegments$ref
+  |},
+  +inbound: ?{|
+    +$fragmentRefs: FlightSegments$ref
+  |},
   +$refType: BoardingPassReturn$ref,
 |};
 */
 
 
-const node/*: ConcreteFragment*/ = {
+const node/*: ConcreteFragment*/ = (function(){
+var v0 = [
+  {
+    "kind": "FragmentSpread",
+    "name": "FlightSegments",
+    "args": null
+  }
+];
+return {
   "kind": "Fragment",
   "name": "BoardingPassReturn",
   "type": "BookingReturn",
@@ -26,12 +39,28 @@ const node/*: ConcreteFragment*/ = {
   "argumentDefinitions": [],
   "selections": [
     {
-      "kind": "FragmentSpread",
-      "name": "OutboundFlights",
-      "args": null
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "outbound",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "Trip",
+      "plural": false,
+      "selections": v0
+    },
+    {
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "inbound",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "Trip",
+      "plural": false,
+      "selections": v0
     }
   ]
 };
+})();
 // prettier-ignore
-(node/*: any*/).hash = '6db725bea412141076147b589d9d82d1';
+(node/*: any*/).hash = '90e4f216e66e99c61c949412630598a1';
 module.exports = node;
