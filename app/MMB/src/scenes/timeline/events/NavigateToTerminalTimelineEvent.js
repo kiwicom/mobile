f38de23baa2f4f15af550d0660710a91714300c6
Robin Cussol <robin@loki.local>
2018-07-17 13:42:51 +0200

    Adding NavigateToTerminal event; fixing icon for DownloadBoardingPass; fixing font color for time

---
+++ app/MMB/src/scenes/timeline/events/NavigateToTerminalTimelineEvent.js
@@ -0,0 +1,40 @@
+// @flow
+
+import * as React from 'react';
+import idx from 'idx';
+import { graphql, createFragmentContainer } from '@kiwicom/mobile-relay';
+import { Translation } from '@kiwicom/mobile-localization';
+import { TextIcon } from '@kiwicom/mobile-shared';
+
+import Title from '../MainContentTitle';
+import type { NavigateToTerminalTimelineEvent as NavigateToTerminalTimelineEventType } from './__generated__/NavigateToTerminalTimelineEvent.graphql';
+import TimelineEvent from '../TimelineEvent';
+import TimelineEventIcon from '../TimelineEventIcon';
+
+type Props = {|
+  +data: NavigateToTerminalTimelineEventType,
+|};
+
+const NavigateToTerminalTimelineEvent = (props: Props) => {
+  const timestamp = idx(props, _ => _.data.timestamp);
+  return (
+    <TimelineEvent
+      timestamp={timestamp}
+      iconVertLines={<TimelineEventIcon icon={<TextIcon code="&#xe052;" />} />}
+      mainContent={
+        <Title>
+          <Translation id="mmb.booking_timeline.event.navigate_to_terminal.title" />
+        </Title>
+      }
+    />
+  );
+};
+
+export default createFragmentContainer(
+  NavigateToTerminalTimelineEvent,
+  graphql`
+    fragment NavigateToTerminalTimelineEvent on NavigateToTerminalTimelineEvent {
+      timestamp
+    }
+  `,
+);
