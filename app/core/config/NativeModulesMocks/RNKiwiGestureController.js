2c6032d1e6c54f81b0f3f804c1bd76e9c34bdcb1
ferrannp <fnp.developer@gmail.com>
2018-07-18 16:52:03 +0200

    fix: Do not allow swipe back on iOS modals.

--- app/core/config/NativeModulesMocks/RNKiwiGestureController.js
+++ app/core/config/NativeModulesMocks/RNKiwiGestureController.js
@@ -5,4 +5,6 @@ import { NativeModules } from 'react-native';
 NativeModules.RNKiwiGestureController = {
   enableGestures: jest.fn((moduleName: string) => moduleName),
   disableGestures: jest.fn((moduleName: string) => moduleName),
+  closeModal: jest.fn(),
+  invokeDefaultBackButton: jest.fn(),
 };
