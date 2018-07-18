4d7783e2de567e543d00213a83e997a8111a4aaa
Trond Bergquist <trond_bergquist@hotmail.com>
2018-07-18 11:22:18 +0200

    Hotels: add letterSpacing to Stars component

--- packages/shared/src/Stars.js
+++ packages/shared/src/Stars.js
@@ -3,11 +3,24 @@
 import * as React from 'react';
 import { Translation } from '@kiwicom/mobile-localization';
 
+import Text from './Text';
+import StyleSheet from './PlatformStyleSheet';
+
 type Props = {|
   // number of stars
   +rating?: ?number,
 |};
 
 export default function Stars({ rating }: Props) {
-  return <Translation passThrough={'★'.repeat(rating || 0)} />;
+  return (
+    <Text style={styles.stars}>
+      <Translation passThrough={'★'.repeat(rating || 0)} />
+    </Text>
+  );
 }
+
+const styles = StyleSheet.create({
+  stars: {
+    letterSpacing: 2,
+  },
+});
