025b4c425d3cda7074c940b4404e00993753eb09
Trond Bergquist <trond_bergquist@hotmail.com>
2018-07-26 12:39:28 +0200

    Hotels: Add ancillaryPurchased logging event when hotel is paid for

--- packages/shared/src/WebView.js
+++ packages/shared/src/WebView.js
@@ -5,13 +5,14 @@ import { WebView as NativeWebView } from 'react-native';
 import { Translation } from '@kiwicom/mobile-localization';
 
 import GeneralError from './errors/GeneralError';
+import type { WebViewStateChangeEvent } from '../types/Events';
 
-type Props = {
-  // not exact - may contain additional properties (extend as needed)
-  source: {|
-    uri: string,
+type Props = {|
+  +source: {|
+    +uri: string,
   |},
-};
+  onNavigationStateChange?: (event: WebViewStateChangeEvent) => void,
+|};
 
 export default class WebView extends React.Component<Props> {
   renderError = () => (
@@ -21,11 +22,18 @@ export default class WebView extends React.Component<Props> {
     />
   );
 
+  onNavigationStateChange = (event: WebViewStateChangeEvent) => {
+    if (this.props.onNavigationStateChange) {
+      this.props.onNavigationStateChange(event);
+    }
+  };
+
   render = () => (
     <NativeWebView
       bounces={false}
       startInLoadingState={false} // it feels faster because we don't have to wait until the page is fully loaded
       renderError={this.renderError}
+      onNavigationStateChange={this.onNavigationStateChange}
       {...this.props}
     />
   );
