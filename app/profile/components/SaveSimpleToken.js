a7787eecb60e8560220621e910f280d67ac32449
Trond Bergquist <trond_bergquist@hotmail.com>
2018-07-30 12:41:51 +0200

    MMB: Prepare List screen for simple token usage

---
+++ app/profile/components/SaveSimpleToken.js
@@ -0,0 +1,51 @@
+// @flow
+
+import * as React from 'react';
+import { View } from 'react-native';
+import { Translation } from '@kiwicom/mobile-localization';
+import { TextButton, TextInput } from '@kiwicom/mobile-shared';
+
+type Props = {|
+  +onSave: (simpleToken: string, bookingId: string) => void,
+|};
+
+type State = {|
+  bookingId: string,
+  simpleToken: string,
+|};
+
+export default class SaveSimpleToken extends React.Component<Props, State> {
+  state = {
+    bookingId: '',
+    simpleToken: '',
+  };
+
+  onBookingIdChange = (bookingId: string) => {
+    this.setState({ bookingId });
+  };
+
+  onSimpleTokenChange = (simpleToken: string) => {
+    this.setState({ simpleToken });
+  };
+
+  onSave = () =>
+    this.props.onSave(this.state.simpleToken, this.state.bookingId);
+
+  render = () => (
+    <View>
+      <TextInput
+        onChangeText={this.onBookingIdChange}
+        placeholder={<Translation passThrough="Booking id" />}
+      />
+      <TextInput
+        onChangeText={this.onSimpleTokenChange}
+        placeholder={<Translation passThrough="simple token" />}
+      />
+
+      <TextButton
+        onPress={this.onSave}
+        title={<Translation passThrough="Use simple login" />}
+      />
+    </View>
+  );
+}
