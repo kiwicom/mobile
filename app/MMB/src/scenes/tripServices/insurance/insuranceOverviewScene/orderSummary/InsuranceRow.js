72a868271b51294b02e199b5ceb522abdb2d1b30
Robin Cussol <robin@loki.home>
2018-08-10 17:06:59 +0200

    Insurance context: order summary

---
+++ app/MMB/src/scenes/tripServices/insurance/insuranceOverviewScene/orderSummary/InsuranceRow.js
@@ -0,0 +1,131 @@
+// @flow
+
+import * as React from 'react';
+import { View } from 'react-native';
+import idx from 'idx';
+import { StyleSheet, Color, Text, Price } from '@kiwicom/mobile-shared';
+import { Translation } from '@kiwicom/mobile-localization';
+
+import { withInsuranceContext } from '../InsuranceOverviewSceneContext';
+
+type InsuranceType = 'NONE' | 'TRAVEL_BASIC' | 'TRAVEL_PLUS';
+
+type Change = {|
+  +databaseId: ?number,
+  +from: ?InsuranceType,
+  +to: ?InsuranceType,
+|};
+
+type InsurancePrice = {|
+  +insuranceType: InsuranceType,
+  +price: {|
+    +amount: number,
+    +currency: string,
+  |},
+|};
+
+type Props = {|
+  +insuranceType: InsuranceType,
+  +insurancePrices: InsurancePrice[],
+  +changes: Change[],
+|};
+
+function InsuranceRow(props: Props) {
+  let translationId = '';
+  switch (props.insuranceType) {
+    case 'TRAVEL_PLUS': {
+      translationId = 'mmb.trip_services.insurance.variant.plus.quantity';
+      break;
+    }
+    case 'TRAVEL_BASIC': {
+      translationId = 'mmb.trip_services.insurance.variant.basic.quantity';
+      break;
+    }
+    case 'NONE':
+    default: {
+      translationId = 'mmb.trip_services.insurance.variant.none.quantity';
+      break;
+    }
+  }
+
+  const { changes, insurancePrices } = props;
+
+  const insuranceQuantity = changes.filter(
+    change => change.to === props.insuranceType && change.from !== change.to,
+  ).length;
+
+  if (insuranceQuantity === 0) {
+    return null;
+  }
+
+  let insuranceCurrency = '';
+  const insuranceAmount = changes
+    .filter(
+      change => change.to === props.insuranceType && change.from !== change.to,
+    )
+    .map(change => {
+      const priceFrom = idx(
+        insurancePrices.find(
+          insurancePrice => insurancePrice.insuranceType === change.from,
+        ),
+        _ => _.price,
+      );
+      const priceTo = idx(
+        insurancePrices.find(
+          insurancePrice => insurancePrice.insuranceType === change.to,
+        ),
+        _ => _.price,
+      );
+      if (priceFrom && priceTo && priceFrom.currency === priceTo.currency) {
+        insuranceCurrency = priceFrom.currency;
+        return priceTo.amount - priceFrom.amount;
+      } else if (priceFrom && priceTo === null) {
+        insuranceCurrency = priceFrom.currency;
+        return -priceFrom.amount;
+      } else if (priceFrom === null && priceTo) {
+        insuranceCurrency = priceTo.currency;
+        return priceTo.amount;
+      }
+      return 0;
+    })
+    .reduce((curr, acc) => curr + acc, 0);
+
+  return (
+    <View style={styleSheet.row}>
+      <View style={styleSheet.item}>
+        <Text style={styleSheet.text}>
+          <Translation
+            id={translationId}
+            values={{ quantity: insuranceQuantity }}
+          />
+        </Text>
+      </View>
+      <Price
+        amount={insuranceAmount}
+        currency={insuranceCurrency}
+        style={styleSheet.price}
+      />
+    </View>
+  );
+}
+
+export default withInsuranceContext(state => ({
+  changes: state.changes,
+  insurancePrices: state.insurancePrices,
+}))(InsuranceRow);
+
+const styleSheet = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginVertical: 15,
+  },
+  item: {
+    flexGrow: 1,
+  },
+  text: {
+    color: Color.textLight,
+  },
+  price: {
+    color: Color.white,
+  },
+});
