0805e738a7c481280ace698cec1a144a5a25f0b6
lukewalczak <lukasz.walczak@callstack.io>
2018-08-13 13:49:00 +0200

    Integrate logic for apple wallet button
    Use default alert button
    
    Revert change in project.pbxproj
    
    Integrate logic for apple wallet button
    
    Use default alert button
    
    Revert change in project.pbxproj
    
    Adjust pass for smaller devices
    
    Merge branch 'feat_apple_wallet_button_logic' of https://github.com/kiwicom/mobile into feat_apple_wallet_button_logic
    
    Resolve errors after merging

--- ios/RNModules/RNAppleWallet/RNAppleWallet/RNKiwiAppleWalletManager.m
+++ ios/RNModules/RNAppleWallet/RNAppleWallet/RNKiwiAppleWalletManager.m
@@ -5,6 +5,29 @@
 
 RCT_EXPORT_MODULE()
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+RCT_EXPORT_METHOD(addPass:(NSString *)url resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    NSError *downloadingError = nil;
+    NSData *data = [NSData dataWithContentsOfURL:[[NSURL alloc] initWithString:url] options:NSDataReadingUncached error:&downloadingError];
+    if (downloadingError) {
+        reject(@"Network error", [downloadingError localizedDescription], downloadingError);
+        return;
+    }
+    
+    NSError *parsingError = nil;
+    PKPass *pass = [[PKPass alloc] initWithData:data error:&parsingError];
+    if (parsingError) {
+        reject(@"Parsing error", [parsingError localizedDescription], parsingError);
+        return;
+    }
+    
+    PKPassLibrary *passLib = [PKPassLibrary new];
+    
+    if (![passLib containsPass:pass]) {
+        PKAddPassesViewController *passVc = [[PKAddPassesViewController alloc] initWithPass:pass];
+        [RCTPresentedViewController() presentViewController:passVc animated:YES completion:nil];
+    }
+    resolve(nil);
+}
 
 - (UIView *)view
 {
