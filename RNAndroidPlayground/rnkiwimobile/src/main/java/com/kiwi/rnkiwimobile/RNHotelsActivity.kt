f79ca4ffb4ba2143cd143d9f2c12d2f9c7fa62e0
Ferran Negre <fnp.developer@gmail.com>
2018-08-24 15:49:23 +0200

    rnkiwimobile extensible for multiple apps
    * chore: rnkiwimobile extensible for multiple apps.
    
    * chore: Inject modules in the constructor.

---
+++ RNAndroidPlayground/rnkiwimobile/src/main/java/com/kiwi/rnkiwimobile/RNHotelsActivity.kt
@@ -0,0 +1,38 @@
+package com.kiwi.rnkiwimobile
+
+import com.airbnb.android.react.maps.MapsPackage
+import com.facebook.react.ReactPackage
+import com.oblador.vectoricons.VectorIconsPackage
+import com.reactlibrary.RNTooltipsPackage
+import com.skypicker.reactnative.nativemodules.currency.CurrencyChangeCallback
+import com.skypicker.reactnative.nativemodules.currency.RNCurrencyManagerPackage
+import com.skypicker.reactnative.nativemodules.device.RNDeviceInfoPackage
+import com.skypicker.reactnative.nativemodules.logging.RNLoggingPackage
+import com.skypicker.reactnative.nativemodules.translation.RNTranslationManagerPackage
+import com.skypicker.reactnative.nativemodules.translation.ResourceStringCallback
+
+interface RNHotelsModulesInjection {
+  val translationCallback: ResourceStringCallback
+  val currencyCallback: CurrencyChangeCallback
+  val hasActiveBooking: Boolean
+}
+
+abstract class RNHotelsActivity(private val rnHotelsModules: RNHotelsModulesInjection) : RNKiwiActivity() {
+  override fun getModuleName(): String {
+    return "KiwiHotels"
+  }
+
+  override fun getJSEntryPoint(): String {
+    return "app/hotels/index"
+  }
+
+  override fun getPackages(): MutableList<ReactPackage> {
+    return mutableListOf(RNTooltipsPackage(),
+      RNDeviceInfoPackage(),
+      MapsPackage(),
+      RNCurrencyManagerPackage(rnHotelsModules.currencyCallback),
+      RNTranslationManagerPackage(rnHotelsModules.translationCallback),
+      RNLoggingPackage(rnHotelsModules.hasActiveBooking),
+      VectorIconsPackage())
+  }
+}
