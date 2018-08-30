b0cb31debb808eb0ddbc742de7de14d2d42e7f2b
Trond Bergquist <trond_bergquist@hotmail.com>
2018-08-30 11:09:21 +0200

    MMB: Fix header share button for e-ticket and boardingPasses

--- app/MMB/src/scenes/tickets/boardingPassesPdf/BoardingPassPdfScreen.js
+++ app/MMB/src/scenes/tickets/boardingPassesPdf/BoardingPassPdfScreen.js
@@ -1,8 +1,71 @@
 // @flow strict
 
 import * as React from 'react';
+import Share from 'react-native-share';
+import { documentDir } from '@kiwicom/mobile-assets';
+import type { NavigationType } from '@kiwicom/mobile-navigation';
+import idx from 'idx';
 
 import BoardingPassesPdf from './BoardingPassPdf';
+import ShareButton from '../../../components/ShareButton';
+import BookingDetailContext from '../../../context/BookingDetailContext';
+
+type ShareButtonPropsWithContext = {|
+  +boardingPassUrl: string,
+  +bookingId: number,
+  +flightNumber: string,
+|};
+
+class BoardingPassShareButton extends React.Component<
+  ShareButtonPropsWithContext,
+> {
+  onPress = () => {
+    this.sharePromise();
+  };
+
+  sharePromise = async () => {
+    try {
+      const boardingPassUrl = `${documentDir}/boardingPasses/${
+        this.props.bookingId
+      }/${this.props.flightNumber}.pdf`;
+
+      await Share.open({ url: boardingPassUrl });
+    } catch (err) {
+      // No worries, user just canceled share
+      return;
+    }
+  };
+
+  render = () => <ShareButton onPress={this.onPress} />;
+}
+
+type ShareButtonProps = {|
+  +navigation?: NavigationType,
+|};
+
+export function BoardingPassShareButtonWithContext(props: ShareButtonProps) {
+  const boardingPassUrl = idx(
+    props,
+    _ => _.navigation.state.params.boardingPassUrl,
+  );
+  const flightNumber = idx(props, _ => _.navigation.state.params.flightNumber);
+
+  if (boardingPassUrl == null || flightNumber == null) {
+    return null;
+  }
+
+  return (
+    <BookingDetailContext.Consumer>
+      {({ bookingId }) => (
+        <BoardingPassShareButton
+          boardingPassUrl={boardingPassUrl}
+          bookingId={bookingId}
+          flightNumber={flightNumber}
+        />
+      )}
+    </BookingDetailContext.Consumer>
+  );
+}
 
 type Props = {|
   +boardingPassUrl: ?string,
