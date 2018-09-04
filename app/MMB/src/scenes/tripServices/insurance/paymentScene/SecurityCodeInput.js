7d34605c1c14ca2799935404844fdb18fe692bc2
Robin Cussol <me@robincussol.com>
2018-09-04 13:48:15 +0200

    Help pop-up for security code on insurance payment screen

--- app/MMB/src/scenes/tripServices/insurance/paymentScene/SecurityCodeInput.js
+++ app/MMB/src/scenes/tripServices/insurance/paymentScene/SecurityCodeInput.js
@@ -1,21 +1,53 @@
 // @flow
 
 import * as React from 'react';
-import { Translation } from '@kiwicom/mobile-localization';
-import { TextInput } from '@kiwicom/mobile-shared';
+import { View, TouchableWithoutFeedback } from 'react-native';
+import { Translation, Alert } from '@kiwicom/mobile-localization';
+import { TextInput, StyleSheet, TextIcon } from '@kiwicom/mobile-shared';
+import { defaultTokens } from '@kiwicom/mobile-orbit';
 
 type Props = {|
   +onSecurityCodeChange: () => void,
 |};
 
-export default function SecurityCodeInput(props: Props) {
-  return (
-    <TextInput
-      label={
-        <Translation id="mmb.trip_services.insurance.payment.security_code" />
-      }
-      onChangeText={props.onSecurityCodeChange}
-      keyboardType="numeric"
-    />
-  );
+export default class SecurityCodeInput extends React.Component<Props> {
+  onPress = () => {
+    Alert.translatedAlert(
+      { id: 'mmb.trip_services.insurance.payment.security_code' },
+      {
+        id: 'mmb.trip_services.insurance.payment.security_code.alert_message',
+      },
+    );
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <TextInput
+          label={
+            <Translation id="mmb.trip_services.insurance.payment.security_code" />
+          }
+          onChangeText={this.props.onSecurityCodeChange}
+          keyboardType="numeric"
+        />
+        <TouchableWithoutFeedback onPress={this.onPress}>
+          <View style={styles.helpIcon}>
+            <TextIcon code="F" style={styles.icon} />
+          </View>
+        </TouchableWithoutFeedback>
+      </React.Fragment>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  helpIcon: {
+    position: 'absolute',
+    end: 15,
+    bottom: 2.5,
+    padding: 10,
+  },
+  icon: {
+    color: defaultTokens.paletteProductNormal,
+    fontSize: 16,
+  },
+});
