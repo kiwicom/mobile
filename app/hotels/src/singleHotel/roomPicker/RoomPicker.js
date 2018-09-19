fd17c70c1a7a3523bf36265385c60a48c05b078d
Trond Bergquist <trond_bergquist@hotmail.com>
2018-09-19 16:38:54 +0200

    Hotels: New design for roompicker button

--- app/hotels/src/singleHotel/roomPicker/RoomPicker.js
+++ app/hotels/src/singleHotel/roomPicker/RoomPicker.js
@@ -1,153 +1,33 @@
 // @flow strict
 
 import * as React from 'react';
-import { View } from 'react-native';
-import {
-  Price,
-  Text,
-  StyleSheet,
-  Touchable,
-  ButtonTitle,
-} from '@kiwicom/mobile-shared';
-import { Translation } from '@kiwicom/mobile-localization';
-import { defaultTokens } from '@kiwicom/mobile-orbit';
 
-type Props = $ReadOnly<{|
+import SelectButton from './SelectButton';
+import IncrementDecrement from './IncrementDecrement';
+
+type Props = {|
   +price: number | null,
   +currency: string | null,
   +selectedCount: number,
   +selectableCount: number,
   +increment: () => void,
   +decrement: () => void,
-|}>;
+|};
 
-export default function RoomPicker({
-  price,
-  currency,
-  selectedCount,
-  selectableCount,
-  increment,
-  decrement,
-}: Props) {
-  if (price == null && currency == null) {
+export default function RoomPicker(props: Props) {
+  if (props.price == null && props.currency == null) {
     return null;
   }
 
-  if (selectedCount === 0) {
+  if (props.selectedCount === 0) {
     return (
-      <Touchable
-        style={[
-          styles.container,
-          styles.borderRadiusLeft,
-          styles.borderRadiusRight,
-        ]}
-        onPress={increment}
-      >
-        <View style={styles.row}>
-          <ButtonTitle
-            style={styles.text}
-            text={<Translation id="single_hotel.room_picker.select" />}
-          />
-          <View style={styles.row} />
-          <Price amount={price} currency={currency} style={styles.text} />
-        </View>
-      </Touchable>
+      <SelectButton
+        price={props.price}
+        currency={props.currency}
+        increment={props.increment}
+      />
     );
   }
 
-  return (
-    <View style={styles.row}>
-      <MinusButton onPress={decrement} />
-
-      <View style={[styles.container, styles.row, styles.priceAndCount]}>
-        <Text style={[styles.text, styles.count]}>
-          <Translation passThrough={`${selectedCount} × `} />
-        </Text>
-        <Price amount={price} currency={currency} style={styles.text} />
-      </View>
-
-      <PlusButton
-        onPress={increment}
-        disabled={selectedCount >= selectableCount}
-      />
-    </View>
-  );
+  return <IncrementDecrement {...props} />;
 }
-
-const MinusButton = ({ onPress }) => {
-  return (
-    <Touchable
-      style={[styles.button, styles.borderRadiusLeft]}
-      onPress={onPress}
-    >
-      <Text style={styles.buttonText}>
-        <Translation passThrough="-" />
-      </Text>
-    </Touchable>
-  );
-};
-
-const PlusButton = ({ disabled, onPress }) => {
-  let disabledStyle;
-  if (disabled === true) {
-    disabledStyle = styles.disabledButton;
-  }
-
-  return (
-    <Touchable
-      style={[styles.button, styles.borderRadiusRight, disabledStyle]}
-      onPress={onPress}
-      disabled={disabled}
-    >
-      <Text style={styles.buttonText}>
-        <Translation passThrough="+" />
-      </Text>
-    </Touchable>
-  );
-};
-
-const radius = 6;
-const styles = StyleSheet.create({
-  row: {
-    flexGrow: 1,
-    flexDirection: 'row',
-  },
-  container: {
-    backgroundColor: defaultTokens.paletteProductLight,
-    height: 44,
-    paddingHorizontal: 10,
-    paddingVertical: 14,
-  },
-  text: {
-    color: defaultTokens.paletteProductDark,
-    fontSize: 14,
-  },
-  button: {
-    backgroundColor: defaultTokens.paletteProductNormal,
-    width: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  disabledButton: {
-    opacity: 0.2,
-  },
-  buttonText: {
-    fontSize: 20,
-    fontWeight: '600',
-    color: defaultTokens.paletteWhite,
-  },
-  priceAndCount: {
-    justifyContent: 'center',
-  },
-  count: {
-    fontWeight: 'bold',
-  },
-  borderRadiusLeft: {
-    borderTopStartRadius: radius,
-    borderBottomStartRadius: radius,
-  },
-  borderRadiusRight: {
-    borderTopEndRadius: radius,
-    borderBottomEndRadius: radius,
-  },
-});
