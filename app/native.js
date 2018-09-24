1c142b024b032fa7d735b2dc197ea4ac04e1a774
Luke Walczak <lukasz.walczak.pwr@gmail.com>
2018-09-24 13:48:51 +0200

    Use live dates instead of fixed (#1123)

--- app/native.js
+++ app/native.js
@@ -1,10 +1,19 @@
 // @flow
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import {
   SingleHotelStandalonePackage,
   NewHotelsStandAlonePackage,
 } from '@kiwicom/react-native-app-hotels';
+
+// TODO: please check if it's still needed
+YellowBox.ignoreWarnings([
+  // react-native-share warnings. Should be gone with the version 1.1.3
+  'Class GenericShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
+  'Class WhatsAppShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
+  'Class GooglePlusShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
+  'Class InstagramShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
+]);
 
 // Hotels
 AppRegistry.registerComponent(
