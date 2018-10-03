f8e9a297af1301957b2f958f6877894023230a5f
lukewalczak <lukewalczak@android-25196ccda9720024.localdomain>
2018-10-03 16:07:37 +0200

    Create two RNNativePlayground schemas

--- ios/RNKiwiMobile/RNKiwiConstants.m
+++ ios/RNKiwiMobile/RNKiwiConstants.m
@@ -6,9 +6,11 @@
 @implementation RNKiwiConstants
 
 + (NSURL *)bundleURL {
-// To run with packager comment line #10 and uncomment line #11
-  return [[NSBundle bundleForClass:[self class]] URLForResource:@"main" withExtension:@"jsbundle"];
-//  return [NSURL URLWithString:@"http://localhost:8081/app/native.bundle?platform=ios&dev=true"];
+  #if DEBUG
+    return [NSURL URLWithString:@"http://localhost:8081/app/native.bundle?platform=ios&dev=true"];
+  #else
+    return [[NSBundle bundleForClass:[self class]] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 }
 
 @end
