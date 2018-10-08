63283580ea67bbf9fc0082ab7400f9d61cb45d5e
ferrannp <fnp.developer@gmail.com>
2018-10-08 16:42:56 +0200

    Do not hardcode dates for the example.

--- android/playground/src/main/java/com/kiwi/rnandroidplayground/HotelsData.kt
+++ android/playground/src/main/java/com/kiwi/rnandroidplayground/HotelsData.kt
@@ -1,17 +1,30 @@
 package com.kiwi.rnandroidplayground
 
+import android.util.Log
 import com.kiwi.rnkiwimobile.hotels.RNHotelsCoordinates
 import com.kiwi.rnkiwimobile.hotels.RNHotelsInitialProperties
 import com.kiwi.rnkiwimobile.hotels.RNHotelsRoomsConfiguration
+import java.text.SimpleDateFormat
+import java.util.*
 
 object HotelsData {
   fun getInitialProperties(): RNHotelsInitialProperties {
+    val checkIn = Calendar.getInstance()
+    checkIn.add(Calendar.DATE, 12)
+
+    val checkOut = Calendar.getInstance()
+    checkOut.add(Calendar.DATE, 14)
+
+    val simpleDateFormat = SimpleDateFormat("YYYY-MM-dd", Locale.US)
+
+    Log.d("HotelsData", simpleDateFormat.format(checkIn.time) + " " + simpleDateFormat.format(checkOut.time))
+
     return RNHotelsInitialProperties(
         "en",
         "EUR",
         "",
-        "2018-12-20",
-        "2018-12-25",
+        simpleDateFormat.format(checkIn.time),
+        simpleDateFormat.format(checkOut.time),
         "Barcelona",
         "aG90ZWxDaXR5Oi0zNzI0OTA",
         RNHotelsRoomsConfiguration(
