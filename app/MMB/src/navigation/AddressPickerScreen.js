7eb5f9ba2eb42cb744f981f149e54d3aefd0e335
Robin Cussol <me@robincussol.com>
2018-10-10 15:42:14 +0200

    Fix: headerStyle removed on TransportationMap screens to prevent odd behaviour on Android

--- app/MMB/src/navigation/AddressPickerScreen.js
+++ app/MMB/src/navigation/AddressPickerScreen.js
@@ -64,7 +64,6 @@ export class AddressPickerScreen extends React.Component<Props, State> {
       headerTitle: (
         <AddressLocationInput autoFocus={true} onChangeText={onChangeText} />
       ),
-      headerStyle: { paddingBottom: 5 },
     };
   };
 
