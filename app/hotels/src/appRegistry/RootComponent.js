ebfcbdbe50d3d51818f2633f31045b8686baa1cb
Trond Bergquist <trond_bergquist@hotmail.com>
2018-10-22 08:58:50 +0200

    Hotels: Remove useless contexts and add search stats to hotels context

--- app/hotels/src/appRegistry/RootComponent.js
+++ app/hotels/src/appRegistry/RootComponent.js
@@ -2,13 +2,8 @@
 
 import * as React from 'react';
 import { ConfigContext } from '@kiwicom/mobile-config';
-import {
-  GeolocationContext,
-  Dimensions,
-  type DimensionType,
-} from '@kiwicom/mobile-shared';
+import { Dimensions, type DimensionType } from '@kiwicom/mobile-shared';
 
-import HotelsSearchContext from '../HotelsSearchContext';
 import HotelsFilterContext from '../HotelsFilterContext';
 import HotelsContext, { type RoomConfigurationType } from '../HotelsContext';
 import SearchResultsContext from '../navigation/allHotels/SearchResultsContext';
@@ -30,25 +25,21 @@ export default class RootComponent extends React.Component<Props> {
   render = () => (
     <SearchResultsContext.Provider>
       <ConfigContext.Provider dataSaverEnabled={this.props.dataSaverEnabled}>
-        <HotelsSearchContext.Provider>
-          <HotelsFilterContext.Provider>
-            <GeolocationContext.Provider>
-              <HotelsContext.Provider
-                version={this.props.version}
-                cityId={this.props.cityId}
-                checkin={this.props.checkin}
-                checkout={this.props.checkout}
-                roomsConfiguration={this.props.roomsConfiguration}
-                currency={this.props.currency}
-                cityName={this.props.cityName}
-              >
-                <Dimensions.Provider dimensions={this.props.dimensions}>
-                  {this.props.children}
-                </Dimensions.Provider>
-              </HotelsContext.Provider>
-            </GeolocationContext.Provider>
-          </HotelsFilterContext.Provider>
-        </HotelsSearchContext.Provider>
+        <HotelsFilterContext.Provider>
+          <HotelsContext.Provider
+            version={this.props.version}
+            cityId={this.props.cityId}
+            checkin={this.props.checkin}
+            checkout={this.props.checkout}
+            roomsConfiguration={this.props.roomsConfiguration}
+            currency={this.props.currency}
+            cityName={this.props.cityName}
+          >
+            <Dimensions.Provider dimensions={this.props.dimensions}>
+              {this.props.children}
+            </Dimensions.Provider>
+          </HotelsContext.Provider>
+        </HotelsFilterContext.Provider>
       </ConfigContext.Provider>
     </SearchResultsContext.Provider>
   );
