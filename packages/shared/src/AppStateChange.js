c606adf97ae437a1f0469da70d0da92976e83e6a
Trond Bergquist <trond_bergquist@hotmail.com>
2018-10-26 10:11:16 +0200

    Upgrade flow

--- packages/shared/src/AppStateChange.js
+++ packages/shared/src/AppStateChange.js
@@ -12,7 +12,7 @@ type AppStateTypes =
   | 'inactive';
 
 type Props = {|
-  children: React.Fragment,
+  children: React.Node | (() => React.Node),
   states: $ReadOnlyArray<AppStateTypes>,
   onStateChange: string => void,
 |};
