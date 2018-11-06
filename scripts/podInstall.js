182422b8098a802338a5dc6f5bb3a993ec44e46b
Ferran Negre <fnp.developer@gmail.com>
2018-11-06 14:36:09 +0100

    Clean up unused scripts

--- scripts/podInstall.js
+++
@@ -1,8 +0,0 @@
-// @flow strict
-
-const path = require('path');
-const exec = require('child_process').execSync;
-
-const iosPath = path.join(__dirname, '..', 'ios');
-
-exec('pod install', { cwd: iosPath, stdio: 'inherit' });
