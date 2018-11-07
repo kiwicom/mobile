c9795aeafe5b65d7e64153bca510a17dd3944687
Trond Bergquist <trond_bergquist@hotmail.com>
2018-11-07 11:40:55 +0100

    Hotels: Create tablet navigation stack

--- app/hotels/src/appRegistry/NewHotelsStandalonePackage.js
+++ app/hotels/src/appRegistry/NewHotelsStandalonePackage.js
@@ -4,10 +4,12 @@ import * as React from 'react';
 import {
   WithNativeNavigation,
   type DimensionType,
+  AdaptableLayout,
 } from '@kiwicom/mobile-shared';
 
 import RootComponent from './RootComponent';
 import HotelsStack from '../navigation/HotelsNavigationStack';
+import HotelsTabletStack from '../navigation/HotelsTabletStack';
 import type { Coordinates } from '../CoordinatesType';
 import type { RoomConfigurationType } from '../HotelsContext';
 
@@ -33,29 +35,35 @@ class NewHotelsStandalonePackage extends React.Component<Props> {
   static navigationOptions = {
     tabBarVisible: false,
   };
-  render = () => (
-    <RootComponent
-      dimensions={this.props.dimensions}
-      dataSaverEnabled={this.props.dataSaverEnabled}
-      version={this.props.version}
-      cityId={this.props.cityId}
-      checkin={this.props.checkin}
-      checkout={this.props.checkout}
-      currency={this.props.currency}
-      roomsConfiguration={this.props.roomsConfiguration}
-      cityName={this.props.cityName}
-      coordinates={this.props.coordinates}
-      // Stay 22 only supports searching with gps coordinates, if we get passed a cityId from native
-      // we are using booking.com, if we don't get cityId, we should have coordinates, and we are using stay22 provider
-      apiProvider={this.props.cityId == null ? 'stay22' : 'booking'}
-    >
-      <HotelsStack
-        screenProps={this.props}
-        onBackClicked={this.props.onBackClicked}
-        onNavigationStateChange={this.props.onNavigationStateChange}
-      />
-    </RootComponent>
-  );
+  render() {
+    const props = {
+      screenProps: this.props,
+      onBackClicked: this.props.onBackClicked,
+      onNavigationStateChange: this.props.onNavigationStateChange,
+    };
+    return (
+      <RootComponent
+        dimensions={this.props.dimensions}
+        dataSaverEnabled={this.props.dataSaverEnabled}
+        version={this.props.version}
+        cityId={this.props.cityId}
+        checkin={this.props.checkin}
+        checkout={this.props.checkout}
+        currency={this.props.currency}
+        roomsConfiguration={this.props.roomsConfiguration}
+        cityName={this.props.cityName}
+        coordinates={this.props.coordinates}
+        // Stay 22 only supports searching with gps coordinates, if we get passed a cityId from native
+        // we are using booking.com, if we don't get cityId, we should have coordinates, and we are using stay22 provider
+        apiProvider={this.props.cityId == null ? 'stay22' : 'booking'}
+      >
+        <AdaptableLayout
+          renderOnNarrow={<HotelsStack {...props} />}
+          renderOnWide={<HotelsTabletStack {...props} />}
+        />
+      </RootComponent>
+    );
+  }
 }
 
 export default WithNativeNavigation(
