d30f0cc5a24d0cedde8469ceb26ec75f2454ebb9
Trond Bergquist <trond_bergquist@hotmail.com>
2018-11-14 12:49:27 +0100

    Replace node-fetch with @mrtnzlml/fetch

--- scripts/getSchema.js
+++ scripts/getSchema.js
@@ -1,29 +1,30 @@
 // @flow strict
 
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
+import fs from 'fs';
+import path from 'path';
+import fetch from '@mrtnzlml/fetch';
+
 const {
   buildClientSchema,
   getIntrospectionQuery,
   printSchema,
 } = require('graphql/utilities');
 
-fetch('https://graphql.kiwi.com/', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    query: getIntrospectionQuery(),
-  }),
-})
-  .then(res => res.json())
-  .then(schemaJSON => printSchema(buildClientSchema(schemaJSON.data)))
-  .then(clientSchema =>
-    fs.writeFileSync(
-      path.join(__dirname, '..', 'schema.graphql'),
-      clientSchema,
-    ),
-  );
+(async () => {
+  const res = await fetch('https://graphql.kiwi.com/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: getIntrospectionQuery(),
+    }),
+    fetchTimeout: 15000,
+    retryDelays: [1000, 3000],
+  });
+
+  const schemaJSON = await res.json();
+  const clientSchema = printSchema(buildClientSchema(schemaJSON.data));
+  fs.writeFileSync(path.join(__dirname, '..', 'schema.graphql'), clientSchema);
+})();
