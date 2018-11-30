325b8b9d3149d366768a69aa21795a88cfb43506
lukewalczak <lukasz.walczak.pwr@gmail.com>
2018-11-30 13:21:14 +0100

    Expose stay22 verion in ios playground

--- ios/RNNativePlayground/City.m
+++ ios/RNNativePlayground/City.m
@@ -2,13 +2,14 @@
 
 @implementation City
 
-- (id) initWithName:(NSString *)cityName andId:(NSString *)cityId
+- (id) initWithName:(NSString *)cityName andId:(NSString *)cityId andCoordinates:(NSDictionary *)cityCoordinates;
 {
   self = [super init];
   if (self)
   {
     self.cityName = cityName;
     self.cityId = cityId;
+    self.cityCoordinates = cityCoordinates;
   }
   return self;
 }
