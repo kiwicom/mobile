89de8d228269377adf46f1a5990dd3dba307e55d
Robin Cussol <me@robincussol.com>
2018-12-17 16:58:54 +0100

    Fix: translation strings compatible with native code implementation

--- packages/localization/src/TranslationHelpers.js
+++ packages/localization/src/TranslationHelpers.js
@@ -7,10 +7,11 @@ export const getTranslation = (
   translationId: TranslationKeys,
 ): string => {
   const nativeKey = `mobile.${translationId}`;
+  const defaultTranslation = DefaultVocabulary[translationId];
 
   if (translatedString === nativeKey) {
     // fallback to our dummy vocabulary because native code didn't provide translation
-    return DefaultVocabulary[translationId];
+    return defaultTranslation;
   }
   if (translatedString == null) {
     // Everything failed - native translation even our fallback. This usually
@@ -18,7 +19,36 @@ export const getTranslation = (
     // we can actually do is to use original key as a text.
     return translationId;
   }
-  return translatedString;
+
+  // Translated strings from native come under the form
+  // %1$@, so we lose information about the variables.
+  // We add this information back in:
+  try {
+    const NATIVE_TRANSLATION_PARAM_REGEX = /%([0-9]+)\$@/g;
+    const compatibleTranslatedString = translatedString.replace(
+      NATIVE_TRANSLATION_PARAM_REGEX,
+      (match, variableIndex) => {
+        const ORIGINAL_PARAM_REGEX = new RegExp(
+          `__(${variableIndex}_)?([a-zA-Z-_]+)__`,
+          'g',
+        );
+        const originalParamMatches = defaultTranslation.match(
+          ORIGINAL_PARAM_REGEX,
+        );
+
+        if (originalParamMatches == null || originalParamMatches.length !== 1) {
+          throw new Error(
+            `There was a problem with translationId ${translationId}`,
+          );
+        }
+
+        return originalParamMatches[0];
+      },
+    );
+    return compatibleTranslatedString;
+  } catch (e) {
+    return translatedString;
+  }
 };
 
 export function replaceValues(text: string, values?: Object): string {
