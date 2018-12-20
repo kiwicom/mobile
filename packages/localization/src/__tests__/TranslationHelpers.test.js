0638af485f697a45059fd085d75ea645abd5a28a
Robin Cussol <me@robincussol.com>
2018-12-20 14:59:21 +0100

    Vocabulary as argument of getTranslation

--- packages/localization/src/__tests__/TranslationHelpers.test.js
+++ packages/localization/src/__tests__/TranslationHelpers.test.js
@@ -1,36 +1,42 @@
 // @flow strict
 
-import DefaultVocabulary from '../DefaultVocabulary';
 import { getTranslation } from '../TranslationHelpers';
+
+const vocabulary = {
+  'single_hotel.hotel_review.reviews': '__count__ Reviews',
+  'hotels.gallery.pagination': '__1_photoNumber__ of __2_totalPhotos__',
+};
 
 describe('getTranslation:', () => {
   it('returns default translation if string is not translated', () => {
     const translationId = 'single_hotel.hotel_review.reviews';
     const notTranslatedKey = 'mobile.single_hotel.hotel_review.reviews';
-    expect(getTranslation(notTranslatedKey, translationId)).toBe(
-      DefaultVocabulary[translationId],
+    expect(getTranslation(notTranslatedKey, translationId, vocabulary)).toBe(
+      '__count__ Reviews',
     );
   });
 
   it('returns translation id if translated string is null/undefined', () => {
     const translationId = 'single_hotel.hotel_review.reviews';
     const notTranslatedKey = null;
-    expect(getTranslation(notTranslatedKey, translationId)).toBe(translationId);
+    expect(getTranslation(notTranslatedKey, translationId, vocabulary)).toBe(
+      translationId,
+    );
   });
 
   it('handles natively translated strings -- with one parameter', () => {
     const translationId = 'single_hotel.hotel_review.reviews';
     const translatedString = '%1$@ Reviews';
-    expect(getTranslation(translatedString, translationId)).toBe(
-      DefaultVocabulary[translationId],
+    expect(getTranslation(translatedString, translationId, vocabulary)).toBe(
+      '__count__ Reviews',
     );
   });
 
   it('handles natively translated strings -- with several parameters', () => {
     const translationId = 'hotels.gallery.pagination';
     const translatedString = '%1$@ of %2$@';
-    expect(getTranslation(translatedString, translationId)).toBe(
-      DefaultVocabulary[translationId],
+    expect(getTranslation(translatedString, translationId, vocabulary)).toBe(
+      '__1_photoNumber__ of __2_totalPhotos__',
     );
   });
 
@@ -38,8 +44,8 @@ describe('getTranslation:', () => {
     const translationId = 'hotels.gallery.pagination';
     const translatedString = '%1$@ of %2$@';
     const mistake = ' and MISTAKE %3$@';
-    expect(getTranslation(translatedString + mistake, translationId)).toBe(
-      translatedString + mistake,
-    );
+    expect(
+      getTranslation(translatedString + mistake, translationId, vocabulary),
+    ).toBe(translatedString + mistake);
   });
 });
