4ca81baa9f3acf628c3630d2a54fa2854134e33f
Trond Bergquist <trond_bergquist@hotmail.com>
2019-01-16 12:54:39 -0500

    Hotels: Use icon component in HotelAmenities
    Refactor component structure for hotel amenities

---
+++ app/hotels/src/filter/hotelAmenities/HotelAmenityCheckbox.js
@@ -0,0 +1,38 @@
+// @flow strict
+
+import * as React from 'react';
+import { View } from 'react-native';
+import { Checkbox, Icon, StyleSheet } from '@kiwicom/mobile-shared';
+import { type TranslationType } from '@kiwicom/mobile-localization';
+
+type Props = {|
+  +isChecked: boolean,
+  +icon: React.Element<typeof Icon>,
+  +onPress: string => void,
+  +amenityName: string,
+  +text: TranslationType,
+|};
+
+export default class HotelAmenityCheckbox extends React.Component<Props> {
+  onPress = () => {
+    this.props.onPress(this.props.amenityName);
+  };
+
+  render() {
+    return (
+      <Checkbox isChecked={this.props.isChecked} onPress={this.onPress}>
+        <View style={styles.checkbox}>
+          {this.props.icon}
+          {this.props.text}
+        </View>
+      </Checkbox>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
