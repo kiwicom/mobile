72edfa50ad47f3c3c2fe194a84058c25460a9e34
Trond Bergquist <trond_bergquist@hotmail.com>
2019-01-22 08:31:29 +0100

    Shared: delete TextIcon file
    New Icon file which uses universal-components should be used instead

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -91,7 +91,6 @@ export {
   default as AppleWalletBackgroundTablet,
 } from './images/apple-wallet-image-2x.jpg';
 
-export { default as TextIcon } from './src/icons/TextIcon'; // Do not use, should be replaced by icon
 export { default as Icon } from './src/icons/Icon';
 
 export { default as NetworkImage } from './src/image/NetworkImage';
