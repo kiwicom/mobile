9956f3fc103fe91d5590006a0e4412a57a3ffb8e
Trond Bergquist <trond_bergquist@hotmail.com>
2019-01-24 08:55:41 +0100

    Add e2e test for not enough beds alert

---
+++ e2e/hotels/hotelDetails/notEnoughBeds.spec.js
@@ -0,0 +1,35 @@
+// @flow
+
+/* eslint-env detox/detox */
+
+import {
+  goToHotelsTab,
+  goToHotelSearchResults,
+  goToFirstHotelDetails,
+  scrollToBottomOfHotelDetails,
+  selectLastRoom,
+  pressBookNow,
+} from '../helpers';
+
+describe('Hotel detail - Not enough beds warning', () => {
+  it('should show an alert when there are not enough beds for all guests', async () => {
+    await goToHotelsTab();
+    await expect(element(by.id('adultsIncrement'))).toBeVisible();
+
+    for (let i = 0; i < 9; i++) {
+      await element(by.id('adultsIncrement')).tap(); // eslint-disable-line no-await-in-loop
+    }
+
+    await goToHotelSearchResults();
+    await goToFirstHotelDetails();
+    await scrollToBottomOfHotelDetails();
+    await selectLastRoom();
+    await pressBookNow();
+
+    await expect(
+      element(
+        by.label("Thats's fine").and(by.type('_UIAlertControllerActionView')),
+      ),
+    ).toBeVisible();
+  });
+});
