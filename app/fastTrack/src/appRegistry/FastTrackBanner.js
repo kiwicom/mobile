6bc042300f403fa0576a9659b5acbb9d07b1d379
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-02-08 11:52:00 +0100

    WIP: Basic banner mock.

--- app/fastTrack/src/appRegistry/FastTrackBanner.js
+++ app/fastTrack/src/appRegistry/FastTrackBanner.js
@@ -1,20 +1,98 @@
-// @flow strict
+// @noflow
 
 import * as React from 'react';
-import { WithNativeNavigation } from '@kiwicom/mobile-shared';
-import { Translation } from '@kiwicom/mobile-localization';
+// eslint-disable-next-line
+import { View } from 'react-native';
+
+import {
+  SimpleCard,
+  Text,
+  CenteredView,
+  TextButton,
+  Modal,
+} from '@kiwicom/mobile-shared';
 
 // This will be prop types passed from native app
-type Props = {||};
+type Props = {|
+  +height: number,
+|};
 
-class FastTrackBanner extends React.Component<Props> {
+type State = {|
+  +isModalVisible: boolean,
+|};
+
+class FastTrackBanner extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isModalVisible: false,
+    };
+  }
+
+  onOpenModal = () => {
+    this.setState({
+      isModalVisible: true,
+    });
+  };
+
+  onCloseModal = () => {
+    this.setState({
+      isModalVisible: false,
+    });
+  };
+
+  renderModal = () => (
+    <Modal
+      isVisible={this.state.isModalVisible}
+      onRequestClose={this.onCloseModal}
+      testId="fastTrackBanner"
+    >
+      <View>
+        <Text
+          style={{
+            marginBottom: 16,
+            textAlign: 'center',
+            fontWeight: 'bold',
+            color: '#006dc7',
+          }}
+        >
+          This is modal content.
+        </Text>
+        <TextButton
+          style={{ flexBasis: 100 }}
+          type="secondary"
+          onPress={this.onCloseModal}
+          title="Close modal"
+        />
+      </View>
+    </Modal>
+  );
+
   render() {
-    return <Translation passThrough="Put code here" />;
+    return (
+      <SimpleCard style={{ height: this.props.height }}>
+        <React.Fragment>
+          <Text
+            style={{
+              marginBottom: 16,
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: '#00a991',
+            }}
+          >
+            Fast Track Banner test
+          </Text>
+          <TextButton
+            type="primary"
+            onPress={this.onOpenModal}
+            title="Open Modal"
+          />
+          {this.renderModal()}
+        </React.Fragment>
+      </SimpleCard>
+    );
   }
 }
 
-// With navigation solves problem with swipe gestures, back button
-// And navigation between native app and RN app.
-// I am not 100% sure you need it if you will just run as a fragment.
-// You can experiment without to to verify
-export default WithNativeNavigation(FastTrackBanner, 'FastTrackBanner');
+export default FastTrackBanner;
