0199d7468c01b4b2e3102b834a85803b76a83f65
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-03-01 11:46:27 +0100

    new: Use requester in AncillaryFactory

--- app/ancillaryFactory/src/appRegistry/AncillaryFactory.js
+++ app/ancillaryFactory/src/appRegistry/AncillaryFactory.js
@@ -3,18 +3,30 @@
 import * as React from 'react';
 import { FastTrackBanner } from '@kiwicom/react-native-fast-track';
 
+// Enums
 import AncillaryServiceType from '../enums/AncillaryServiceType';
+
+// Services
+import requester from '../services/requester';
+
+// Components
 import NoAncillaryMessage from '../components/NoAncillaryMessage';
 
 type Props = {|
   +serviceName: string,
+  +token: string,
   +bookingId: number,
 |};
 
 const AncillaryFactory = (props: Props) => {
+  const ancillaryProps = {
+    bookingId: props.bookingId,
+    requester: requester(props.token),
+  };
+
   switch (props.serviceName) {
     case AncillaryServiceType.FAST_TRACK:
-      return <FastTrackBanner bookingId={props.bookingId} />;
+      return <FastTrackBanner {...ancillaryProps} />;
     default:
       return <NoAncillaryMessage />;
   }
