9adf72690238be5ea10f9b739876cd5bfe04ffc9
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-03-05 19:31:14 +0100

    Separate CodePush deploy for ancillaries.

--- scripts/codePush.js
+++ scripts/codePush.js
@@ -7,27 +7,41 @@ const childProcess = require('child_process');
 
 const packageJson = require('../package.json');
 
+const isRunningForAncillaries = process.argv.slice(2)[0] === '--ancillaries';
+
 const exec = (command, options) =>
   childProcess.execSync(command, {
     stdio: 'inherit',
     ...options,
   });
 
-const targetBinaryVersion =
+let targetBinaryVersion =
   packageJson.rnkiwimobile['code-push-target-binary-version'];
 
-const entryFile = path.join(__dirname, '..', 'app/native.js');
+let entryFile = path.join(__dirname, '..', 'app/native.js');
+
+let targetProjectAndroid = 'Kiwicom/mobile-android';
+let targetProjectIOS = 'Kiwicom/mobile-ios';
+
+// If --ancillaries flag is passed override entry file, target binary version and target project
+if (isRunningForAncillaries) {
+  entryFile = path.join(__dirname, '../app/ancillaries-native.js');
+  targetBinaryVersion =
+    packageJson.rnkiwimobile['code-push-ancillaries-target-binary-version'];
+  targetProjectAndroid = 'Kiwicom/mobile-android-ancillaries';
+  targetProjectIOS = 'Kiwicom/mobile-ios-ancillaries';
+}
 
 function processCommand() {
   const target = 'Staging';
   console.log(`Building for ${target}...`);
 
   exec(
-    `appcenter codepush release-react -a Kiwicom/mobile-android --entry-file ${entryFile} --target-binary-version "${targetBinaryVersion}" -d ${target}`,
+    `appcenter codepush release-react -a ${targetProjectAndroid} --entry-file ${entryFile} --target-binary-version "${targetBinaryVersion}" -d ${target}`,
   );
 
   exec(
-    `appcenter codepush release-react -a Kiwicom/mobile-ios --entry-file ${entryFile} --target-binary-version "${targetBinaryVersion}" -d ${target}`,
+    `appcenter codepush release-react -a ${targetProjectIOS} --entry-file ${entryFile} --target-binary-version "${targetBinaryVersion}" -d ${target}`,
   );
 }
 
