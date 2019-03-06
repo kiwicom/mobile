95a14f2cc3b4e3c858c7932e1046a0259802ad89
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-03-06 16:37:13 +0100

    Added AncillaryConstants file.

---
+++ ios/RNKiwiMobile/AncillaryConstants.h
@@ -0,0 +1,17 @@
+//
+//  AncillaryConstants.h
+//  RNKiwiMobile
+//
+//  Copyright © 2019 Kiwi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AncillaryConstants;
+
+@interface AncillaryConstants: NSObject;
+
++ (NSURL *)bundleURL;
++ (NSString *)codePushAppVersion;
+
+@end
---
+++ ios/RNKiwiMobile/AncillaryConstants.m
@@ -0,0 +1,26 @@
+//
+//  AncillaryConstants.m
+//  RNKiwiMobile
+//
+//  Copyright © 2019 Kiwi. All rights reserved.
+//
+
+#import "AncillaryConstants.h"
+#import <React/RCTBundleURLProvider.h>
+#import <CodePush/CodePush.h>
+
+@implementation AncillaryConstants
+
++ (NSURL *)bundleURL {
+#ifdef DEBUG
+  return [NSURL URLWithString:@"http://localhost:8081/app/native.bundle?platform=ios&dev=true"];
+#else
+  return [CodePush bundleURLForResource:@"ancillaries" withExtension:@"jsbundle" subdirectory:nil bundle:[NSBundle bundleWithIdentifier:@"com.kiwi.RNKiwiMobile"]];
+#endif
+}
+
++ (NSString *)codePushAppVersion {
+  return @"2.0.0";
+}
+
+@end
