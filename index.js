c060aad0c564e7c2c1c898085a1f02f438455fd7
Trond Bergquist <trond_bergquist@hotmail.com>
2019-03-28 14:54:30 +0100

    Delete unnecessary YellowBox ignoreWarnings

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ import { AppRegistry, YellowBox } from 'react-native';
 
 // TODO: please check if it's still needed
 YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated in plain JavaScript React classes.',
-  'Module R', // ... requires main queue setup since it overrides ...
-  'Class RCTCxxModule was not exported. Did you forget to use RCT_EXPORT_MODULE()?', // https://github.com/facebook/react-native/issues/18201
   // TODO this a warning from react-navigation, see: https://reactnavigation.org/docs/en/common-mistakes.html
   // This happens in our app because the package-stack architecture we have for native apps
   'You should only render one navigator',
-  'Class GenericShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
-  'Class WhatsAppShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
-  'Class GooglePlusShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
-  'Class InstagramShare was not exported. Did you forget to use RCT_EXPORT_MODULE()',
 ]);
 
 import App from './app/App';
