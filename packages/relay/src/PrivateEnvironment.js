c2c70e00dc2ea4650f3e27f7c84bb45478b97b84
Trond Bergquist <trond_bergquist@hotmail.com>
2019-04-10 11:15:03 +0200

    Relay: Allow changing the authorization header key
    We have different header keys for authorization, so we need to be able
    to change this header value based on which query we are calling

--- packages/relay/src/PrivateEnvironment.js
+++ packages/relay/src/PrivateEnvironment.js
@@ -6,13 +6,13 @@ export default new class PrivateEnvironment {
   environment = null;
   token = null;
 
-  getEnvironment = (token: string) => {
+  getEnvironment = (token: string, headerKey?: string) => {
     if (!token) {
       throw new Error('Cannot create private environment without token.');
     }
 
     if (this.environment === null || token !== this.token) {
-      this.environment = createEnvironment(token);
+      this.environment = createEnvironment(token, headerKey);
     }
 
     this.token = token;
