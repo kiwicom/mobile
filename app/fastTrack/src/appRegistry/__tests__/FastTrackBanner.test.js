81fa219257150139946cac7fa0e1c8231ad2c66b
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-04-17 13:09:53 +0200

    Ancillaries: Shallow rendering testfor FastTrackBanner

--- app/fastTrack/src/appRegistry/__tests__/FastTrackBanner.test.js
+++ app/fastTrack/src/appRegistry/__tests__/FastTrackBanner.test.js
@@ -1,15 +1,16 @@
 // @flow
 
 import React from 'react';
-import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 import FastTrackBanner from '../FastTrackBanner';
 
 describe('FastTrackBanner', () => {
   test('Should render correctly', () => {
-    const component = renderer.create(
-      <FastTrackBanner bookingId={123} kwAuthToken="x.y.z" />,
-    );
-    expect(component).toMatchSnapshot();
+    const renderer = new ShallowRenderer();
+
+    renderer.render(<FastTrackBanner bookingId={123} kwAuthToken="x.y.z" />);
+
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
   });
 });
