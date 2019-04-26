d3eec39844d16e6f8c2ee7beb3f7c91e034edd84
Vedran Blazenka <1723170+vblazenka@users.noreply.github.com>
2019-04-26 09:35:12 +0200

    Ancillaries: Renamed translation keys

--- packages/localization/src/vocabularies/FastTrack.js
+++ packages/localization/src/vocabularies/FastTrack.js
@@ -1,15 +1,20 @@
 // @flow strict
 
 const FastTrackVocabulary = {
-  'fast_track.banner.title': 'Milan Bergamo Stopover Bundle',
-  'fast_track.banner.description':
+  'mmb.fast_track.banner.title.bergamo.bergamo_stopover':
+    'Milan Bergamo Stopover Bundle',
+  'mmb.fast_track.banner.description.bergamo.bergamo_stopover':
     'VIP treatment is waiting for you at Milan Bergamo Airport with Fast Track and baggage assistance.',
-  'fast_track.banner.services_label': 'Services included',
-  'fast_track.banner.services_security_route': 'Fast track security route',
-  'fast_track.banner.services_handling_assistance':
+  'mmb.fast_track.banner.services_label.bergamo.bergamo_stopover':
+    'Services included',
+  'mmb.fast_track.banner.services_security_route.bergamo.bergamo_stopover':
+    'Fast track security route',
+  'mmb.fast_track.banner.services_handling_assistance.bergamo.bergamo_stopover':
     'Baggage handling assistance',
-  'fast_track.banner.show_qr_button': 'Show QR Code',
-  'fast_track.banner.modal.close_modal_button': 'Close modal',
+  'mmb.fast_track.banner.show_qr_button.bergamo.bergamo_stopover':
+    'Show QR Code',
+  'mmb.fast_track.banner.close_modal_button.bergamo.bergamo_stopover':
+    'Close modal',
 };
 
 export default FastTrackVocabulary;
