261f0cd3742b7852bcad9e983d2d676b83c84fb8
Trond Bergquist <trond_bergquist@hotmail.com>
2019-07-02 08:06:41 +0200

    Upgrade native-dependencies
    react-native-maps
    react-native-gesture-handler
    netinfo
    react-native-webview
    async-storage

--- app/core/config/NativeModulesMocks/ReactNativeGestureHandler.js
+++ app/core/config/NativeModulesMocks/ReactNativeGestureHandler.js
@@ -4,17 +4,16 @@ import { NativeModules } from 'react-native';
 
 NativeModules.UIManager = NativeModules.UIManager ?? {};
 NativeModules.UIManager.RCTView = NativeModules.UIManager.RCTView ?? {};
-NativeModules.RNGestureHandlerModule = NativeModules.RNGestureHandlerModule ?? {
+NativeModules.PlatformConstants = {};
+NativeModules.RNGestureHandlerModule = {
+  Direction: {},
+  createGestureHandler: () => {},
+  attachGestureHandler: () => {},
   State: {
     BEGAN: 'BEGAN',
     FAILED: 'FAILED',
     ACTIVE: 'ACTIVE',
     END: 'END',
   },
-};
-NativeModules.PlatformConstants = {};
-NativeModules.RNGestureHandlerModule = {
-  Direction: {},
-  createGestureHandler: () => {},
-  attachGestureHandler: () => {},
+  dropGestureHandler: () => {},
 };
