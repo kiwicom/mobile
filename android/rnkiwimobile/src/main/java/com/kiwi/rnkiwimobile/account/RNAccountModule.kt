42f314e19b06e4a995a1085d8aac45fa21669321
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-23 11:49:18 +0200

    Link NetInfo and AsyncStorage Packge into Account module
    remove unused reference to RNCWebViewPackage

--- android/rnkiwimobile/src/main/java/com/kiwi/rnkiwimobile/account/RNAccountModule.kt
+++ android/rnkiwimobile/src/main/java/com/kiwi/rnkiwimobile/account/RNAccountModule.kt
@@ -3,7 +3,6 @@ package com.kiwi.rnkiwimobile.account
 import android.os.Bundle
 import com.facebook.react.ReactPackage
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
@@ -11,7 +10,10 @@ object RNAccountModule {
     const val jsEntryPoint = "app/native.js"
 
     fun getPackages(): MutableList<ReactPackage> {
-        return mutableListOf(RNGestureHandlerPackage())
+        return mutableListOf(
+            RNGestureHandlerPackage(),
+            NetInfoPackage(),
+            AsyncStoragePackage())
     }
 
     fun getInitialProperties(initialProperties: RNAccountInitialProperties): Bundle? {
