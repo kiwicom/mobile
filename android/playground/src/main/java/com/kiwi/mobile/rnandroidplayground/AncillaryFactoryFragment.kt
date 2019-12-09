8333a08d8e65b07264bcd19f26be00df053ff23e
KarinaDavtyan <k.davtyan27@gmail.com>
2019-12-09 16:07:29 +0100

    chore: add ends  of lines and make lines shorter

--- android/playground/src/main/java/com/kiwi/mobile/rnandroidplayground/AncillaryFactoryFragment.kt
+++ android/playground/src/main/java/com/kiwi/mobile/rnandroidplayground/AncillaryFactoryFragment.kt
@@ -5,6 +5,8 @@ import com.kiwi.rnkiwimobile.ancillaries.RNAncillaryFactoryFragment
 import com.kiwi.rnkiwimobile.ancillaries.RNAncillaryFactoryInitialProperties
 
 
-class AncillaryFactoryFragment: RNAncillaryFactoryFragment(RNAncillaryFactoryInitialProperties(service = "fast_track", bookingId = 123, kwAuthToken = "mock")) {
+class AncillaryFactoryFragment: RNAncillaryFactoryFragment(
+        RNAncillaryFactoryInitialProperties(service = "fast_track", bookingId = 123, kwAuthToken = "mock")) {
+
     override fun getReactNativeHost(): ReactNativeHost = (activity?.application as PlaygroundApplication).reactNativeHost
 }
--- android/rnkiwimobile/src/main/java/com/kiwi/rnkiwimobile/ancillaries/RNAncillaryFactoryFragment.kt
+++ android/rnkiwimobile/src/main/java/com/kiwi/rnkiwimobile/ancillaries/RNAncillaryFactoryFragment.kt
@@ -2,7 +2,10 @@ package com.kiwi.rnkiwimobile.ancillaries
 
 import com.kiwi.rnkiwimobile.RNKiwiFragment;
 
-abstract  class RNAncillaryFactoryFragment(initialProperties: RNAncillaryFactoryInitialProperties) : RNKiwiFragment(RNAncillaryFactoryModule.getInitialProperties(initialProperties)) {
+abstract  class RNAncillaryFactoryFragment(
+    initialProperties: RNAncillaryFactoryInitialProperties) : RNKiwiFragment(
+        RNAncillaryFactoryModule.getInitialProperties(initialProperties)) {
+
     override fun getModuleName(): String {
         return RNAncillaryFactoryModule.moduleName
     }
