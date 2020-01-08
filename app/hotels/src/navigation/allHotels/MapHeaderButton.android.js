fb8642407cc7ade3be33eb7179ab5143ac38d731
Trond Bergquist <trond_bergquist@hotmail.com>
2020-01-08 09:32:59 +0100

    Add tooltip component from universal-components to shared
    Use tooltip component in MapHeaderButton.android.js

--- app/hotels/src/navigation/allHotels/MapHeaderButton.android.js
+++ app/hotels/src/navigation/allHotels/MapHeaderButton.android.js
@@ -2,8 +2,7 @@
 
 import * as React from 'react';
 import { HeaderButton } from '@kiwicom/mobile-navigation';
-import { Icon, Text, StyleSheet, type TranslationType } from '@kiwicom/mobile-shared';
-import { Tooltip } from '@kiwicom/universal-components';
+import { Icon, Text, StyleSheet, type TranslationType, Tooltip } from '@kiwicom/mobile-shared';
 import { defaultTokens } from '@kiwicom/mobile-orbit';
 
 type Props = {|
